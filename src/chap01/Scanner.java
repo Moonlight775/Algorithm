d3f64d0e4744f1381a0f7a8e21045ef23da8c5eb
KwanYoung Moon <pianoguys775@gmail.com>
2021-11-22 23:01:15 +0900

    next method in Scanner class

---
+++ src/chap01/Scanner.java
@@ -0,0 +1,15 @@
+package chap01;
+
+public class Scanner {
+
+	// nextBoolean() - boolean
+	// nextByte() - byte
+	// nextShort() - short
+	// nextInt() - int
+	// nextLong() - long
+	// nextFloat() - float
+	// nextDouble() - double
+	// next() - String (문자열(스페이스, 줄 바꿈 문자로 구분))
+	// nextLine() - String (문자열 1줄)
+
+}
