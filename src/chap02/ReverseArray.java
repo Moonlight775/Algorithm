30f95df8be1017310bfba2c5976f95ac04908638
KwanYoung Moon <pianoguys775@gmail.com>
2022-01-09 21:50:02 +0900

    file test

--- src/chap02/ReverseArray.java
+++ src/chap02/ReverseArray.java
@@ -35,6 +35,7 @@ public class ReverseArray {
         System.out.println("요소를 역순으로 정렬했습니다.");
         for (int i=0; i<num; i++){
             System.out.println("x[" + i + "] = " + x[i]);
+
         }
     }
 }
