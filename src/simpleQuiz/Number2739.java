be721fafe98c9bf4ea183a27d07d60b9c984a643
KwanYoung Moon <pianoguys775@gmail.com>
2022-02-17 19:26:04 +0900

    backjoon 2739 - for문 이용 간단한 구구단 구현

---
+++ src/simpleQuiz/Number2739.java
@@ -0,0 +1,15 @@
+package simpleQuiz;
+
+import java.util.Scanner;
+
+public class Number2739 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i=1; i<10; i++) {
+            System.out.println(N + " * " + i + " = " + N*i);
+        }
+    }
+}
