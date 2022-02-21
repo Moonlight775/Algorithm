3b153e877a93e0518d414b3ad44e446cc5193f37
Moon <pianoguys775@gmail.com>
2022-02-21 18:15:20 +0900

    백준 2884 - 분이 45보다 작으면 시를 -1 & 분은 60-(45-분) 을 해주어야한다. 또한 시가 0보다 작다면 23시로 변환해준다.

---
+++ src/backjoon/Number2884.java
@@ -0,0 +1,37 @@
+package backjoon;
+
+import java.util.Scanner;
+
+public class Number2884 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int H = in.nextInt(); // 시
+        int M = in.nextInt(); // 분
+        in.close();
+
+        if(M < 45) {
+            H--;		// 시(hour) 1 감소
+            M= 60 - (45 - M); 	// 분(min) 감소
+            if(H < 0) {
+                H = 23;
+            }
+            System.out.println(H + " " + M);
+        }
+        else {
+            System.out.println(H + " " + (M - 45));
+        }
+    }
+}
+
+        //        Scanner sc = new Scanner(System.in);
+//
+//        int H = sc.nextInt();
+//        int M = sc.nextInt();
+//
+//        if (M<=45) H = H-1;
+//        if (H<=0 && M<=45) H = 23;
+//
+//        M = M-45 < 0 ? (M-45)+60 : M-45;
+//
+//        System.out.println(H + " " + M);
