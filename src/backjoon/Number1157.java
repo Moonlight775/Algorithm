a1e564ef7ea373019d8615515d21291da4b66b32
KwanYoung Moon <pianoguys775@gmail.com>
2022-03-01 21:44:30 +0900

    백준 1157 - 입력 받은 단어의 스펠링들에 해당하는 숫자를 arr 배열에 담아준다. - 배열 중에 가장 큰 숫자를 찾고 그 숫자에 해당하는 알파벳을 출력한다. - 가장 큰 숫자가 중복이 있을 경우는 물음표를 출력한다.

---
+++ src/backjoon/Number1157.java
@@ -0,0 +1,34 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number1157 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[26];
+        String str = br.readLine();
+        str = str.toUpperCase();
+
+        for (int i=0; i<str.length(); i++) {
+            if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z') {  // 소문자 범위
+                arr[str.charAt(i) - 'A' ]++; // 해당하는 대문자 인덱스 증가
+            }
+        }
+
+        int max = -1;
+        char ch = '?';
+
+        for (int i=0; i<26; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                ch = (char)(i+65);  // 대문자로 출력하기 위해 65를 더한다
+            } else if (arr[i] == max) {
+                ch = '?';
+            }
+        }
+        System.out.println(ch);
+    }
+}
