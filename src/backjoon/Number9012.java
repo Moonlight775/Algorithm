8db4fd072897f8a83da916ffea09a1ef3326eb3c
KwanYoung Moon <pianoguys775@gmail.com>
2022-03-03 22:54:37 +0900

    Remove .idea from repo

---
+++ src/backjoon/Number9012.java
@@ -0,0 +1,37 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number9012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+
+
+        for (int i=0; i<T; i++) {
+            String str = br.readLine();
+            int lef = 0;
+            int rig = 0;
+
+            for (int j=0; j<str.length(); j++) {
+                if (str.charAt(j) == '(') {
+                    lef++;
+                } else {
+                    rig++;
+                }
+            }
+
+            if (lef != rig) {
+                sb.append("NO").append('\n');
+            } else {
+                sb.append("YES").append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+}
