8988d9ed37402f8373c8cac59f3fd0f92d7ca53b
KwanYoung Moon <pianoguys775@gmail.com>
2022-03-04 23:04:48 +0900

    백준 2480 - 경우의 수를 줄이고 3개의 주사위가 모두 다른 수인 경우 Math.max()를 사용하여 한 줄로 정리

--- src/backjoon/Number2480.java
+++ src/backjoon/Number2480.java
@@ -13,7 +13,18 @@ public class Number2480 {
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
-        int result = 0;
+
+        if (a == b && b == c) {
+            System.out.println(10000 + a*1000);
+        } else if (a == b || a == c) {
+            System.out.println(1000 + a*100);
+        } else if (b == c) {
+            System.out.println(1000 + b*100);
+        } else {
+            System.out.println(Math.max(Math.max(a, b), c) *100);
+        }
+
+        /*int result = 0;
         int max = 0;
 
         if (a == b && b == c) {
@@ -34,6 +45,6 @@ public class Number2480 {
             }
             result = max*100;
         }
-        System.out.println(result);
+        System.out.println(result);*/
     }
 }
