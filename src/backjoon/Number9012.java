98cf21dbca90fb2b082d9e95bebff56c058e4e9d
KwanYoung Moon <pianoguys775@gmail.com>
2022-03-05 16:46:23 +0900

    백준 9012 - '(' 여는 괄호가 있으면 push()하고 닫는 괄호 ')' 가 있으면 pop()하여 최종적으로 스택이 비어있으면 괄호의 쌍이 맞는 것이므로 YES 출력

--- src/backjoon/Number9012.java
+++ src/backjoon/Number9012.java
@@ -3,6 +3,7 @@ package backjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Number9012 {
     public static void main(String[] args) throws IOException {
@@ -11,28 +12,34 @@ public class Number9012 {
 
         int T = Integer.parseInt(br.readLine());
 
-
-
         for (int i=0; i<T; i++) {
-            String str = br.readLine();
-            int lef = 0;
-            int rig = 0;
-
-            for (int j=0; j<str.length(); j++) {
-                if (str.charAt(j) == '(') {
-                    lef++;
-                } else {
-                    rig++;
-                }
-            }
-
-            if (lef != rig) {
-                sb.append("NO").append('\n');
-            } else {
-                sb.append("YES").append('\n');
-            }
+            sb.append(solve(br.readLine())).append('\n');
         }
+
         System.out.println(sb);
+
+    }
+
+    public static String solve(String s) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i=0; i<s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(') {
+                stack.push(c);
+            } else if (stack.empty()) {
+                return "NO";
+            } else {
+                stack.pop();
+            }
+
+        }
+        if (stack.empty()) {
+            return "YES";
+        } else {
+            return "NO";
+        }
     }
 
 }
