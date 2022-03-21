f12bfe6cf204acc5b82dcf60be9c77eea291b12d
KwanYoung Moon <pianoguys775@gmail.com>
2022-03-21 11:36:52 +0900

    백준 8958 - 입력받은 문자열을 배열에 담고 하나씩 꺼내가며 값을 구한다. - 'O'라면 count를 세주고 아니라면 0으로 리셋하며 합산해준다.

--- src/backjoon/Number8958.java
+++ src/backjoon/Number8958.java
@@ -9,14 +9,18 @@ public class Number8958 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
+        String[] str = new String[N];
 
         for (int i=0; i<N; i++) {
-            String str = br.readLine();
+            str[i] = br.readLine();
+        }
+
+        for (int i=0; i<N; i++) {
             int cnt = 0;
             int sum = 0;
 
-            for (int j=0; j<str.length(); j++) {
-                if (str.charAt(j) == 'O') {
+            for (int j=0; j<str[i].length(); j++) {
+                if (str[i].charAt(j) == 'O') {
                     cnt++;
                 } else {
                     cnt = 0;
