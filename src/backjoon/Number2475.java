37aa4a05ac7fb8413de961f76b5dcdba91744bde
Moon <pianoguys775@gmail.com>
2022-03-30 17:42:09 +0900

    입력받은 값을 일일히 변수에 담지 않고 for문을 사용하여 result에 바로 합해준다.

--- src/backjoon/Number2475.java
+++ src/backjoon/Number2475.java
@@ -1,2 +1,16 @@
-package backjoon;public class Number2475 {
+package backjoon;
+
+import java.util.Scanner;
+
+public class Number2475 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+
+        for (int i=0; i<5; i++) {
+            int tmp = sc.nextInt(); // 입력받은 수
+            result += Math.pow(tmp, 2); // 입력받은 수를 Math.pow()를 사용하여 제곱해준 후 result에 더해준다.
+        }
+        System.out.println(result%10);
+    }
 }
