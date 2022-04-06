48d5b953dfbd766ec92b8a77b8320c17b7e825e9
Moon <pianoguys775@gmail.com>
2022-04-06 16:54:41 +0900

    백준2420 - Math.abs()를 사용하여 절대값을 구한다 - 합의 값은 int 범위를 넘으므로 long타입으로 받아준다.

---
+++ src/backjoon/Number2420.java
@@ -0,0 +1,31 @@
+package backjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Number1009 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int ch = 1;
+
+            for(int j=0; j<b; j++){
+                
+            }
+
+        }
+        System.out.println(sb);
+    }
+}
