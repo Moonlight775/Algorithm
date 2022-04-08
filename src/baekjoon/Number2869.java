94a51346d278daeeb04356bbc78a6ecac6d8baeb
KwanYoung Moon <pianoguys775@gmail.com>
2022-04-08 21:45:23 +0900

    백준 2869 - 반복문이 아닌 수식으로 만들어 시간제한에 걸리지 않도록 한다. - 자세한 설명은 주석 참조

--- src/baekjoon/Number2869.java
+++ src/baekjoon/Number2869.java
@@ -10,20 +10,20 @@ public class Number2869 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-        int A = Integer.parseInt(st.nextToken());   // 낮에 올라가는 높이
-        int B = Integer.parseInt(st.nextToken());   // 밤에 내려가는 높이
-        int V = Integer.parseInt(st.nextToken());   // 나무 막대의 높이
-        int result = 0; // 움직이는 높이를 체크하기 위한 변수
-        int cnt = 0;    // 며칠이 걸리는지 확인하기 위한 변수
+        double A = Integer.parseInt(st.nextToken());   // 낮에 올라가는 높이
+        double B = Integer.parseInt(st.nextToken());   // 밤에 내려가는 높이
+        double V = Integer.parseInt(st.nextToken());   // 나무 막대의 높이
 
-        while(true) {
-            result += A;    // 낮에 올라가는 만큼 합한다.
-            cnt++;          // 날짜를 +1 해준다.
-            if(result >= V) break;  // 올라간 높이가 나무 막대의 높이보다 갖거나 높으면 정상에 올라온 것이므로 while문을 종료한다.
+        int X = (int)Math.ceil((V-B) / (A-B));  // 올라가는데 걸리는 일 수
+        // 낮에 정상에 도착한다면 미끄러지지 않으므로 밤에 내려가는 것은 무시하면 된다.
+        // 그렇기 때문에 기준을 낮으로 둔다.
+        // 낮 기준으로 4일차 낮을 예로 들면 A-B+A-B+A-B+A가 된다.
+        // 수식으로 바꿔본다면 X일차일 경우 (A-B)*(X-1)+A 라는 수식이 된다.
+        // V = AX-A-BX+B+A -> AX-BX+B
+        // V-B = AX-BX = (A-B)X
+        // X = (V-B)/(A-B)
+        // X의 값이 소수라면 그 소수만큼 더 가야한다는 뜻으로 올림을 해준다.
 
-            result -= B;    // 밤에 내려가는 만큼 빼준다.
-
-        }
-        System.out.println(cnt);
+        System.out.println(X);
     }
 }
