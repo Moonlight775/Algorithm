9cd4623dc24275f81b0bcb47df2d0448e72fd8b1
KwanYoung Moon <pianoguys775@gmail.com>
2022-04-26 22:34:48 +0900

    백준 2743 - 문자열 길이를 length()를 사용하여 출력

---
+++ src/baekjoon/Number2743.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine().length()); // 문자열의 길이 출력
+    }
+}
