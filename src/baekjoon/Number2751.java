d07a6dc2d8aade527c1c4b967fd0a683bbdbc9cd
KwanYoung Moon <pianoguys775@gmail.com>
2022-05-05 09:01:59 +0900

    백준 2751 - Collections.sort() 사용 - Timsort로 합병 및 삽입정렬 알고리즘을 사용 (primitive 배열이 아닌 List계열의 자료구조를 사용하여 정렬해야한다.) - 상세한 설명은 노션 참조

--- src/baekjoon/Number2751.java
+++ src/baekjoon/Number2751.java
@@ -3,23 +3,28 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Number2751 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int N = Integer.parseInt(br.readLine());    // 수의 개수
-        int[] arr = new int[N];
+
+        ArrayList<Integer> list = new ArrayList<>();    // list 계열 중 하나를 쓰면 된다.
 
         for (int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());   // 입력되는 수로 배열 초기화
+            list.add(Integer.parseInt(br.readLine()));  // 입력되는 수로 list 추가
         }
 
-        Arrays.sort(arr);   // 오름차순 정렬
+        Collections.sort(list); // Collections에서 제공하는 sort 사용
 
-        for (int i : arr) {
-            System.out.println(i);  // 정렬된 배열 출력
+        for (int i : list) {
+            sb.append(i).append('\n');
         }
+
+        System.out.println(sb);
     }
 }
