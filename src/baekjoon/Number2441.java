9739294e7e472588a1f5c8482597448ae46d21d4
KwanYoung Moon <pianoguys775@gmail.com>
2022-05-13 14:23:31 +0900

    백준 2441 - repeat() 메서드만 사용 - 별의 개수는 동일하게 N-i로 결정됨

--- src/baekjoon/Number2441.java
+++ src/baekjoon/Number2441.java
@@ -10,12 +10,8 @@ public class Number2441 {
         int N = sc.nextInt();
 
         for (int i=0; i<N; i++) {
-            sb.append(" ".repeat(i));   // 공백 개수, i의 수만큼 공백 증가
-
-            for (int j=N-i; j>0; j--) { // 별의 개수는 N-i로 정해짐
-                sb.append("*");
-            }
-            sb.append("\n");
+            sb.append(" ".repeat(i));       // 공백 개수, i의 수만큼 공백 증가
+            sb.append("*".repeat(N - i)).append('\n');   // 별의 개수는 N-i로 정해짐
         }
 
         System.out.println(sb);
