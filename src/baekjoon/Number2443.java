4460c258715e8e1c97121aef9689525615f5870b
KwanYoung Moon <pianoguys775@gmail.com>
2022-05-15 09:39:07 +0900

    백준 2443 - repeat() 메서드 사용 - 조건은 for문 사용 시와 같다.

--- src/baekjoon/Number2443.java
+++ src/baekjoon/Number2443.java
@@ -11,13 +11,11 @@ public class Number2443 {
 
         for (int i=N; i>=1; i--) {          // i를 감소식으로 하여 별의 수가 줄어들고, 공백이 늘도록 한다.
 
-            for (int k=N-i; k>0; k--) {     // 공백의 수는 N-i
-                sb.append(" ");
-            }
+            // 공백의 수는 N-i
+            sb.append(" ".repeat(N - i));
 
-            for (int j=0; j<(2*i)-1; j++) { // 별의 수는 (2Xi)-1
-                sb.append("*");
-            }
+            // 별의 수는 (2Xi)-1
+            sb.append("*".repeat((2 * i) - 1));
 
             sb.append('\n');
         }
