56c11951c9b120bda2ea1eef6e10cdf04c2f0f13
KwanYoung Moon <pianoguys775@gmail.com>
2022-06-03 20:06:22 +0900

    백준 11651 - Comparator를 람다식으로 변환

--- src/baekjoon/Number11651.java
+++ src/baekjoon/Number11651.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Number11651 {
@@ -20,20 +19,17 @@ public class Number11651 {
             arr[i][1] = Integer.parseInt(st.nextToken());       // y좌표
         }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[1] == o2[1]) {       // index 1에 담겨 있는 y좌표가 같다면
-                    return o1[0] - o2[0];   // index 0에 담겨 있는 x좌표의 오름차순으로 정렬
-                } else {
-                    return o1[1] - o2[1];   // y좌표의 오름차순 정렬
-                }
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[1] == o2[1]) {       // index 1에 담겨 있는 y좌표가 같다면
+                return o1[0] - o2[0];   // index 0에 담겨 있는 x좌표의 오름차순으로 정렬
+            } else {
+                return o1[1] - o2[1];   // y좌표의 오름차순 정렬
             }
         });
 
         StringBuilder sb = new StringBuilder();
         for (int i=0; i<N; i++) {
-            sb.append(arr[i][0] + " " + arr[i][1]).append('\n');
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append('\n');
         }
 
         System.out.println(sb);
