4e850aa637e92abbfc2508a68df34d36ec7e6419
KwanYoung Moon <pianoguys775@gmail.com>
2022-06-06 21:56:54 +0900

    백준 5554 - 분과 초를 얻기 위해 나누기와 나머지 이용

---
+++ src/baekjoon/Number5554.java
@@ -0,0 +1,20 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number5554 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int sum = 0;
+        
+        for (int i=0; i<4; i++) {
+            sum += Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(sum/60);     // 분
+        System.out.println(sum%60);     // 초
+    }
+}
