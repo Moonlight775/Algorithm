5998f9c57115c5ab58bdab989fbd29c4662bfdc4
KwanYoung Moon <pianoguys775@gmail.com>
2022-06-07 16:41:11 +0900

    백준 2750 - 카운팅 정렬(Counting Sort)를 이용 - boolean 배열을 생성하여 입력받은 값+1000 (절대값이 1000보다 작거나 같은 정수이므로 +1000)을 index로 하여 true로 변경 - boolean 배열 중 true인 것만 순차적으로 출력

--- src/baekjoon/Number2750.java
+++ src/baekjoon/Number2750.java
@@ -9,25 +9,21 @@ public class Number2750 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
 
+        boolean[] arr = new boolean[2001];  // 절대값이 1000보다 작거나 같은 정수이므로 범위 : -1000 ~ 1000
+
+        StringBuilder sb = new StringBuilder();
         for (int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            arr[Integer.parseInt(br.readLine()) + 1000] = true;     // 입력 값이 -1000이라고 한다면 0번째 index가 true로 변환
         }
 
-        for (int i=0; i<N; i++) {
-            for (int j=i+1; j<N; j++) {
-                if (arr[i] > arr[j]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[i];
-                    arr[i] = tmp;
-                }
+        // 정렬 과정이 따로 필요 없음
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i]) {   // i가 0부터 순차대로 늘어나므로 정렬된 듯이 출력된다. ex) -1000, -999 라면 index 0, index 1가 true로 순차적으로 출력가능
+                sb.append(i-1000).append('\n');
             }
         }
 
-        for (int i : arr) {
-            System.out.println(i);
-        }
-
+        System.out.println(sb);
     }
 }
