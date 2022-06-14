07b6873bf0e3f31de4227800cf176d41309a354f
KwanYoung Moon <pianoguys775@gmail.com>
2022-06-14 22:43:45 +0900

    백준 1037 - 약수들이 주어졌을 때 어떤 수 N의 약수인지 구한다. - 주어진 약수들의 최소값과 최대값을 곱하면 N이 구해진다.

---
+++ src/baekjoon/Number1037.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Number1037 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int number = Integer.parseInt(br.readLine());   // 약수의 개수
+        int max = Integer.MIN_VALUE;    // 정수 최소값, -2147483648
+        int min = Integer.MAX_VALUE;    // 정수 최대값, 2147483647
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i=0; i<number; i++) {
+            int A = Integer.parseInt(st.nextToken());   // 진짜 약수
+
+            max = A > max ? A : max;    // 첫 약수는 정수 최소값보다 크므로 max가 초기화된다.
+            min = A < min ? A : min;    // 첫 약수는 정수 최대값보다 크므로 min가 초기화된다.
+        }
+
+        System.out.println(min * max);
+    }
+}
