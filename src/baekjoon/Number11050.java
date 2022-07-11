c9432e8a20d30c98a6a56c2da258f2e555e28974
KwanYoung Moon <pianoguys775@gmail.com>
2022-07-11 09:40:25 +0900

    백준 11050 - 동적계획법을 이용하여 이미 담아져있는 배열 값을 재활용하여 시간 복잡도를 줄인다.

--- src/baekjoon/Number11050.java
+++ src/baekjoon/Number11050.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Number11050 {
+
+    static int[][] dp;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -13,13 +16,20 @@ public class Number11050 {
         int N = Integer.parseInt(st.nextToken());    // 자연수 N
         int K = Integer.parseInt(st.nextToken());    // 정수 K
 
-        // nCk = N! / (N-K)!K!
-        System.out.println(factorial(N) / (factorial(N-K) * factorial(K)));
+        dp = new int[N+1][K+1];
+
+        System.out.println(BC(N,K));
     }
 
-    static int factorial(int N) {
-        if (N <= 1) return 1;   // 0! = 1
+    static int BC(int n, int k) {
+        if (dp[n][k] > 0) {     // 이미 풀었던 sub문제일 경우 값을 재활용
+            return dp[n][k];
+        }
 
-        return N * factorial(N-1);  // 재귀
+        if (k == 0 || n == k) {     // nC0 = nCn = 1
+            return dp[n][k] = 1;
+        }
+
+        return dp[n][k] = BC(n-1, k-1) + BC(n-1, k);    // nCr = n-1Cr-1 + n-1Cr (파스칼의 법칙)
     }
 }
