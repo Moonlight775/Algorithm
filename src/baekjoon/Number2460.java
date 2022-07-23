ebf03a231b6a268698540d43c1b321cb56566525
KwanYoung Moon <pianoguys775@gmail.com>
2022-07-23 19:12:31 +0900

    백준 2460 - 2455번과 동일하나 역이 10개로 증가

---
+++ src/baekjoon/Number2460.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Number2460 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int sum = 0;
+        int max = 0;
+
+        for (int i=0; i<10; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int out = Integer.parseInt(st.nextToken());     // 내린 사람 수
+            int enter = Integer.parseInt(st.nextToken());   // 탄 사람 수
+
+            sum = sum - out + enter;    // 기차 안의 사람 수
+
+            max = Math.max(max, sum);   // max는 가장 많은 사람이 탔을 경우
+        }
+
+        System.out.println(max);
+    }
+}
