488eabcf8face17d9bfafee801b12778dff42c36
KwanYoung Moon <pianoguys775@gmail.com>
2022-08-02 18:50:23 +0900

    백준 1463 - 재귀 방식 - 각 부분에 이전 재귀호출 중 최솟값에 1을 더한 값이 현재 N에 대한 최소연산 횟수가 된다.

---
+++ src/baekjoon/Number1463.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number1463 {
+
+    static Integer[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        dp = new Integer[N+1];
+        dp[0] = dp[1] = 0;
+
+        System.out.println(recur(N));
+    }
+
+    static int recur(int N) {
+        if (dp[N] == null) {
+            // 6으로 나눠질 때 (3, 2 둘 다 나눠질 때)
+            if (N%6 == 0) {
+                dp[N] = Math.min(recur(N-1), Math.min(recur(N/3), recur(N/2))) + 1;
+            }
+            // 3으로만 나눠질 때
+            else if (N%3 == 0) {
+                dp[N] = Math.min(recur(N/3), recur(N-1)) + 1;
+            }
+            // 2로만 나눠질 때
+            else if (N%2 == 0) {
+                dp[N] = Math.min(recur(N/2), recur(N-1)) + 1;
+            }
+            // 2와 3으로 나눠지지 않을 때
+            else {
+                dp[N] = recur(N-1) + 1;
+            }
+        }
+
+        return dp[N];
+    }
+}
