f0d344fb297e0ff713b503db7ebc1b9c64c619e5
KwanYoung Moon <pianoguys775@gmail.com>
2022-08-11 16:24:46 +0900

    백준 25304 - 반복문을 통해 간단히 가격 비교

---
+++ src/baekjoon/Number25304.java
@@ -0,0 +1,32 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Number25304 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int X = Integer.parseInt(br.readLine());    // 총 금액
+        int N = Integer.parseInt(br.readLine());    // 구매한 물건 종류의 개수
+        int sum = 0;    // 물건의 가격 합
+
+        for (int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());   // 물건의 가격
+            int b = Integer.parseInt(st.nextToken());   // 물건의 개수
+
+            sum += a * b;   // 가격 X 개수 = 산 물건의 가격
+        }
+
+        if (X == sum) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+
+    }
+}
