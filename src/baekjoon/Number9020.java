6e2b667e9ea1b7b19f1c71775142f320c0a76ed7
KwanYoung Moon <pianoguys775@gmail.com>
2022-08-16 18:30:12 +0900

    백준 9020 - 수정 완료 - 에라토스테네스의 체를 이용해 소수를 구하는 것은 동일 - 골드바흐 파티션을 구하기 위해서 n을 반으로 나누어 front와 back으로 구분 - front는 -1씩, back은 +1씩 해주면서 두 값이 모두 소수인 것을 찾는다. - 동일하게 -1, +1 해줌으로 n을 벗어날 일은 없다. - 두 소수의 차이가 가장 작은 것을 출력해야 하는데, 위 방법을 사용하면 가장 작은 차이가 출력되고 종료된다.

--- src/baekjoon/Number9020.java
+++ src/baekjoon/Number9020.java
@@ -3,51 +3,52 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Number9020 {
+
+    static boolean[] prime = new boolean[10001];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        boolean[] prime = new boolean[10001];
-        prime[0] = prime[1] = true;
-
-        for (int i=2; i< prime.length; i++) {
-            if (prime[i]) {
-                continue;
-            }
-
-            for (int j=i*i; j<prime.length; j=j+i) {
-                prime[j] = true;
-            }
-        }
-
-        ArrayList<Integer> list = new ArrayList<>();
-
-        for (int i=2; i< prime.length; i++) {
-            if (!prime[i]) {
-                list.add(i);
-            }
-        }
-
-        int T = Integer.parseInt(br.readLine());
-
         StringBuilder sb = new StringBuilder();
-        for (int i=0; i<T; i++) {
-            int n = Integer.parseInt(br.readLine());
 
-            for (int j=0; j<n-3; j++) {
-                for (int k=0; k<n-2; k++) {
-                    if (n == list.get(j) + list.get(k)) {
-                        sb.append(list.get(j)).append(" ").append(list.get(k)).append('\n');
-                        break;
-                    }
+        getPrime();
+
+        int T = Integer.parseInt(br.readLine());        // 테스트 케이스의 개수
+
+        for (int i=0; i<T; i++) {
+            int n = Integer.parseInt(br.readLine());    // 주어진 짝수
+            // n을 반으로 나누어 front는 -1씩 해주고 back은 +1씩 해주면서 그 값이 소수가 되면 출력
+            int front = n / 2;
+            int back = n / 2;
+
+            while (true) {
+                if (!prime[front] && !prime[back]) {    // prime 배열의 index가 false라면 소수라는 뜻
+                    sb.append(front).append(" ").append(back).append('\n');
+                    break;
                 }
+                front--;
+                back++;
             }
 
         }
 
         System.out.println(sb);
-
     }
+
+    // 에라토스테네스의 체를 이용해 소수 구하기
+    public static void getPrime() {
+        prime[0] = prime[1] = true;
+
+        for (int i=2; i<=Math.sqrt(prime.length); i++) {
+            if (prime[i]) {
+                continue;
+            }
+
+            for (int j=i*i; j< prime.length; j=j+i) {
+                prime[j] = true;
+            }
+        }
+    }
+
 }
