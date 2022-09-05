4cbfbe87a35508c6e925107482fb9bbcf2b582a1
KwanYoung Moon <pianoguys775@gmail.com>
2022-09-05 22:33:27 +0900

    백준 2530 - LocalTime 클래스 사용 - plusSeconds() 메소드로 요리 하는데 필요한 초를 더함 - LocalTime이 제공하는 getHour(), getMinute(), getSecond()를 사용하여 시, 분, 초 출력

--- src/baekjoon/Number2530.java
+++ src/baekjoon/Number2530.java
@@ -3,6 +3,7 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.LocalTime;
 import java.util.StringTokenizer;
 
 public class Number2530 {
@@ -15,13 +16,10 @@ public class Number2530 {
         int C = Integer.parseInt(st.nextToken());   // 초
         int D = Integer.parseInt(br.readLine());    // 요리 하는데 필요한 시간 (초)
 
-        C += D;         // 초 + 요리하는데 필요한 시간(초) (ex. C = 0, D = 200 -> C = 200)
-        B += C / 60;    // 합산된 초를 60으로 나눠주면 분이 됨 (ex. B = 30 -> B = 30 + 200/60 = 33)
-        C %= 60;        // 합산된 초에 60으로 나눈 나머지가 최종 초가 됨 (ex. C = 200 -> C = 200%60 = 20)
-        A += B / 60;    // 만약 분이 60분이 넘었다면 60으로 나눠진 값이 + 시가 됨 (ex. A = 14 -> A = 14 + 33/60 = 14)
-        B %= 60;        // 합산된 분에 60으로 나눈 나머지가 최종 분이 됨 (ex. B = 33 % 60 = 33)
-        A %= 24;        // 24시가 오버된 경우 0부터 다시 시작하도록 (ex. A = 14 % 24 = 14)
+        LocalTime localTime  = LocalTime.of(A, B, C);   // 입력된 시, 분, 초로 설정
 
-        System.out.println(A + " " + B + " " + C);
+        localTime = localTime.plusSeconds(D);   // plusSeconds()메소드로 요리 하는데 필요한 초를 더해준다.
+
+        System.out.println(localTime.getHour() + " " + localTime.getMinute() + " " + localTime.getSecond());
     }
 }
