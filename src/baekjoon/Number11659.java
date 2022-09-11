d42d52e1dfb3620ca42eeddde2c17bccd917d015
KwanYoung Moon <pianoguys775@gmail.com>
2022-09-11 14:03:08 +0900

    백준 11659 (구간 합 구하기4) - 배열을 초기화 할 때부터 누적합을 해주면서 초기화 해준다. - i~j번째 까지의 합은 j - (i-1)의 누적합 된 배열 값과 같다.

--- src/baekjoon/Number11659.java
+++ src/baekjoon/Number11659.java
@@ -14,27 +14,22 @@ public class Number11659 {
         int N = Integer.parseInt(st.nextToken());   // 수의 개수
         int M = Integer.parseInt(st.nextToken());   // 합을 구해야하는 횟수
 
-        int[] arr = new int[N];     // 수를 담을 배열
+        int[] arr = new int[N+1];     // 수를 담을 배열
 
-        // 배열 초기화
+        // 배열 초기화 (누적합을 해주면서 초기화)
         st = new StringTokenizer(br.readLine());
-        for (int i=0; i<N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for (int i=1; i<=N; i++) {
+            arr[i] = arr[i-1] + Integer.parseInt(st.nextToken());
         }
 
-        // 누적합 하기
+        // 누적합이 된 배열에서 i번째 부터 j번째까지의 값을 구한다.
         while (M-- > 0) {
             st = new StringTokenizer(br.readLine());
 
             int i = Integer.parseInt(st.nextToken());
             int j = Integer.parseInt(st.nextToken());
-            int sum = 0;
 
-            for (int k=i-1; k<j; k++) {
-                sum += arr[k];
-            }
-
-            sb.append(sum).append('\n');
+            sb.append(arr[j] - arr[i-1]).append('\n');
         }
 
         System.out.println(sb);
