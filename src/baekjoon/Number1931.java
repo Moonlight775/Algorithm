09bbba6ec44c7c29f94bd624c35d7b5b1229c922
KwanYoung Moon <pianoguys775@gmail.com>
2022-09-24 21:09:18 +0900

    백준 1931 (회의실배정, 그리디 알고리즘) - 정렬 시 끝나는 시간이 같을 경우 시작 시간이 빠른순으로 정렬도록 추가 - 정렬 람다식으로 변경 - 이전 끝나는 시간을 변수로 두어서, 다음 회의 시작 시간과 비교로 이중 for문 필요 X

--- src/baekjoon/Number1931.java
+++ src/baekjoon/Number1931.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Number1931 {
@@ -17,31 +16,30 @@ public class Number1931 {
         for (int i=0; i<N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            arr[i][0] = Integer.parseInt(st.nextToken());   // 시작시간
+            arr[i][1] = Integer.parseInt(st.nextToken());   // 끝나는 시간
         }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return o1[1] - o2[1];
+        Arrays.sort(arr, (o1, o2) -> {
+            // 끝나는 시간이 같을 경우 시작시간이 빠른순으로 정렬
+            if (o1[1] == o2[1]) {
+                return o1[0] - o2[0];
             }
+
+            return o1[1] - o2[1];
         });
 
         int count = 0;
-        int change = 0;
+        int prevEndTime = 0;
 
-        for (int i=0; i<N-1; i=change) {
-
-            for (int j=i+1; j<N; j++) {
-                if (arr[i][1] <= arr[j][0]) {
-                    count++;
-                    change = j;
-                    break;
-                }
+        for (int i=0; i<N; i++) {
+            // 직전 끝나는 시간이 다음 회의 시작 시간보다 작거나 같다면 갱신
+            if (prevEndTime <= arr[i][0]) {
+                prevEndTime = arr[i][1];
+                count++;
             }
         }
 
-        System.out.println(count+1);
+        System.out.println(count);
     }
 }
