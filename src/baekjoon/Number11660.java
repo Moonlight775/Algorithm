719bddd7992a5c70dbb87dfbc0f305111321b306
KwanYoung Moon <pianoguys775@gmail.com>
2022-09-26 06:48:50 +0900

    백준 11660 (구간 합 구하기5, 누적합) - 동적계획법 사용으로 시간 단축 - 행 별로 누적합을 하도록 초기화 - 구하는 범위는 열을 기준으로 행을 잘라 합을 누적

--- src/baekjoon/Number11660.java
+++ src/baekjoon/Number11660.java
@@ -13,13 +13,13 @@ public class Number11660 {
         int N = Integer.parseInt(st.nextToken());   // 표의 크기
         int M = Integer.parseInt(st.nextToken());   // 합을 구해야 하는 횟수
 
-        int[][] arr = new int[N][N];    // 표
+        int[][] arr = new int[N+1][N+1];    // 표
 
-        // 표 초기화
-        for (int i=0; i<N; i++) {
+        // 표 초기화 (행 별로
+        for (int i=1; i<=N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j=0; j<N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for (int j=1; j<=N; j++) {
+                arr[i][j] = arr[i][j-1] + Integer.parseInt(st.nextToken());
             }
         }
 
@@ -27,17 +27,14 @@ public class Number11660 {
         while (M-- > 0) {
             st = new StringTokenizer(br.readLine());
 
-            // index가 0부터 시작하므로 좌표값에 -1씩 해줌
-            int x1 = Integer.parseInt(st.nextToken()) -1;
-            int y1 = Integer.parseInt(st.nextToken()) -1;
-            int x2 = Integer.parseInt(st.nextToken()) -1;
-            int y2 = Integer.parseInt(st.nextToken()) -1;
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
             int sum = 0;
 
             for (int i=x1; i<=x2; i++) {
-                for (int j=y1; j<=y2; j++) {
-                    sum += arr[i][j];
-                }
+                sum += arr[i][y2] - arr[i][y1-1];
             }
 
             sb.append(sum).append('\n');
