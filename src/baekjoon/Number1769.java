f12790724a48f797f9fe79df044618e343d47c15
KwanYoung Moon <pianoguys775@gmail.com>
2022-10-12 12:51:35 +0900

    백준 1769 (3의 배수, 문자열) - 자연수를 String으로 받아서 따로 길이를 구하지 않는다. - 홀수 조건을 나머지로 판단

--- src/baekjoon/Number1769.java
+++ src/baekjoon/Number1769.java
@@ -8,25 +8,23 @@ public class Number1769 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int X = Integer.parseInt(br.readLine());
-        int count = 0;  // 반복 횟수
+        String X = br.readLine();
+        int count = 0;  // 반복횟수
 
-        while (X/10 != 0) {     // 일의 자리 수가 아니라면 반복
-            int length = (int)(Math.log10(X)+1);    // 정수 X의 길이
+        while (X.length() != 1) {
             int num = 0;
 
-            // 각 자리수 합 해주기
-            for (int i=0; i<length; i++) {
-                num += X%10;
-                X = X/10;
+            for (int i=0; i<X.length(); i++) {
+                num += X.charAt(i) - '0';
             }
 
-            X = num;
+            X = String.valueOf(num);
             count++;
         }
 
         System.out.println(count);
-        if (X == 3 || X == 6 || X == 9) {
+        // 홀수인지 아닌지 판단
+        if (Integer.parseInt(X) % 3 == 0) {
             System.out.println("YES");
         }
         else {
