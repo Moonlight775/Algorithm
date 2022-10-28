5100346d3470bbfe5d8c45544c67d5fdb52a84dd
KwanYoung Moon <pianoguys775@gmail.com>
2022-10-28 11:53:41 +0900

    Doubly LinkedList 구현 요소를 삭제하는 remove 메소드 구현 - remove() : 가장 앞의 요소(head)를 삭제 - remove(int index) : 특정 index의 요소를 삭제 - remove(Object value) : 특정 요소를 삭제

--- src/list/DLinkedList.java
+++ src/list/DLinkedList.java
@@ -2,6 +2,8 @@ package list;
 
 import Interface_form.List;
 
+import java.util.NoSuchElementException;
+
 public class DLinkedList<E> implements List<E> {
 
     private Node<E> head;   // 노드의 첫 부분
@@ -130,6 +132,148 @@ public class DLinkedList<E> implements List<E> {
         size++;
     }
 
+    public E remove() {
+
+        Node<E> headNode = head;
+
+        if (headNode == null) {
+            throw new NoSuchElementException();
+        }
+
+        // 삭제된 노드를 반환하기 위한 임시 변수
+        E element = headNode.data;
+
+        // head의 다음 노드
+        Node<E> nextNode = head.next;
+
+        // head 노드의 데이터들을 모두 삭제
+        head.data = null;
+        head.next = null;
+
+        /*
+        * head의 다음노드(=nextNode)가 null이 아닐 경우에만
+        * prev 변수를 null로 업데이트 해주어야 한다.
+        * 이유는 nextNode가 없는 경우(null)는 데이터가
+        * 아무 것도 없던 상태였으므로 nextNode.prev를 하면 잘못된 참조가 된다.
+        * */
+        if (nextNode != null) {
+            nextNode.prev = null;
+        }
+
+        head = nextNode;
+        size--;
+
+        /*
+        * 삭제된 요소가 리스트의 유일한 요소였을 경우
+        * 그 요소는 head 이자 tail이었으므로
+        * 삭제되면서 tail도 가리킬 요소가 없기 때문에
+        * size가 0일 경우 tail도 null로 변환
+        * */
+        if (size == 0) {
+            tail = null;
+        }
+
+        return element;
+    }
+
+    @Override
+    public E remove(int index) {
+
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        // 삭제하려는 노드가 첫 번째 노드일 경우
+        if (index == 0) {
+            E element = head.data;
+            remove();
+            return element;
+        }
+
+        Node<E> prevNode = search(index - 1);   // 삭제할 노드의 이전 노드
+        Node<E> removedNode = prevNode.next;         // 삭제할 노드
+        Node<E> nextNode = removedNode.next;         // 삭제할 노드의 다음 노드
+
+        E element = removedNode.data;   // 삭제되는 노드의 데이터를 반환하기 위한 임시 변수
+
+        /*
+        * index == 0 일 때의 조건에서 이미 head노드의 삭제에 대한 분기가 있기 때문에
+        * prevNode는 항상 존재한다.
+        *
+        * 그러나 nextNode의 경우는 null일 수 있기 때문에 (= 마지막 노드를 삭제하려는 경우)
+        * 이전처럼 반드시 검사를 해준 뒤, nextNode.prev에 접근해야 한다.
+        * */
+
+        prevNode.next = null;
+        removedNode.next = null;
+        removedNode.prev = null;
+        removedNode.data = null;
+
+        if (nextNode != null) {
+            nextNode.prev = null;
+
+            nextNode.prev = prevNode;
+            prevNode.next = nextNode;
+        }
+        /*
+        * nextNode가 null이라는 것은 마지막 노드를 삭제했다는 의미이므로
+        * prevNode가 tail이 된다. (연결해줄 것이 없음)
+        * */
+        else {
+            tail = prevNode;
+        }
+
+        size--;
+
+        return element;
+    }
+
+    @Override
+    public boolean remove(Object value) {
+        Node<E> prevNode = head;
+        Node<E> x = head;   // removedNode
+
+        // value와 일치하는 노드를 찾는다.
+        for (; x!= null; x=x.next) {
+            if (value.equals(x.data)) {
+                break;
+            }
+            prevNode = x;
+        }
+
+        // 일치하는 요소가 없을 경우 false 반환
+        if (x == null) {
+            return false;
+        }
+
+        // 삭제하려는 노드가 head일 경우 remove()로 삭제
+        if (x.equals(head)) {
+            remove();
+            return true;
+        }
+        // remove(int index)와 같은 메커니즘으로 삭제
+        else {
+            Node<E> nextNode = x.next;
+
+            prevNode.next = null;
+            x.data = null;
+            x.next = null;
+            x.prev = null;
+
+            if (nextNode != null) {
+                nextNode.prev = null;
+
+                nextNode.prev = prevNode;
+                prevNode.next = nextNode;
+            }
+            else {
+                tail = prevNode;
+            }
+
+            size--;
+            return true;
+        }
+    }
 }
 
 
