123d61eb16704751720cc0538702c2bfdb39099b
KwanYoung Moon <pianoguys775@gmail.com>
2022-10-28 15:39:27 +0900

    Doubly LinkedList 구현 - size() : 리스트에 담긴 요소의 개수 확인 - isEmpty() : 리스트가 비었는지 안 비었는지 판단 - clear() : 리스트 초기화

--- src/list/DLinkedList.java
+++ src/list/DLinkedList.java
@@ -316,6 +316,29 @@ public class DLinkedList<E> implements List<E> {
     public boolean contains(Object item) {
         return indexOf(item) >= 0;
     }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        for (Node<E> x=head; x!=null;) {
+            Node<E> nextNode = x.next;
+            x.data = null;
+            x.next = null;
+            x.prev = null;
+            x = nextNode;
+        }
+        head = tail = null;
+        size = 0;
+    }
 }
 
 
