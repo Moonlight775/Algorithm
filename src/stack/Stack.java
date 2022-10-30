6848c6d8e20bb9b7d0288ec10e4bb9545ea55b04
KwanYoung Moon <pianoguys775@gmail.com>
2022-10-30 16:19:46 +0900

    Stack 구현 [동적할당을 위한 resize 메소드 구현] - 빈 배열일 경우 최소용적(DEFAULT_CAPACITY)로 배열 생성 - 용적이 꽉 찰 경우 용적을 2배로 설정 후 Arrays.copyOf() 메소드를 사용하여 배열 복사 - 데이터가 용적의 절반 미만일 경우는 용적을 1/2로 설정 후 Arrays.copyOf() 메소드를 사용하여 배열 복사

--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -2,6 +2,8 @@ package stack;
 
 import Interface_form.StackInterface;
 
+import java.util.Arrays;
+
 public class Stack<E> implements StackInterface<E> {
 
     private static final int DEFAULT_CAPACITY = 10; // 최소(기본) 용적 크기
@@ -20,5 +22,33 @@ public class Stack<E> implements StackInterface<E> {
     public Stack(int capacity) {
         this.array = new Object[capacity];
         this.size = 0;
+    }
+
+    private void resize() {
+        // 빈 배열일 경우 (capacity is 0)
+        if (Arrays.equals(array, EMPTY_ARRAY)) {
+            array = new Object[DEFAULT_CAPACITY];
+            return;
+        }
+
+        int arrayCapacity = array.length;   // 현재 용적 크기
+
+        //용적이 가득 찰 경우
+        if (size == arrayCapacity) {
+            int newSize = arrayCapacity * 2;
+
+            // 배열 복사
+            array = Arrays.copyOf(array, newSize);
+            return;
+        }
+
+        // 용적의 절반 미만으로 요소가 차지하고 있을 경우
+        if (size < (arrayCapacity / 2)) {
+            int newCapacity = (arrayCapacity / 2);
+
+            // 배열 복사
+            array = Arrays.copyOf(array, Math.max(DEFAULT_CAPACITY, newCapacity));
+            return;
+        }
     }
 }
