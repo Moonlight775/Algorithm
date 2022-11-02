8a61d519ec635cef56f6fa359af2094cb60a7750
KwanYoung Moon <pianoguys775@gmail.com>
2022-11-02 19:48:27 +0900

    Stack sort() 테스트 - Comparator의 구현을 통해 명시적으로 Arrays.sort()에 파라미터로 넘기는 방법

--- src/stack/StackTest.java
+++ src/stack/StackTest.java
@@ -1,5 +1,7 @@
 package stack;
 
+import java.util.Comparator;
+
 public class StackTest {
     public static void main(String[] args) {
         Stack<Student> stack = new Stack<>();
@@ -9,12 +11,20 @@ public class StackTest {
         stack.push(new Student("최신숙", 99));
         stack.push(new Student("최동하", 66));
 
-        stack.sort();
+        stack.sort(customComp); // Comparator를 넘겨준다.
 
         for (Object a : stack.toArray()) {
             System.out.println(a);
         }
     }
+
+    // 사용자 설정 comparator(비교기)
+    static Comparator<Student> customComp = new Comparator<Student>() {
+        @Override
+        public int compare(Student o1, Student o2) {
+            return o2.score - o1.score;
+        }
+    };
 }
 
 class Student {
