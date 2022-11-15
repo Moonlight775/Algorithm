19bdcfd91a2f76769a9613154e9f35e53d6f8bad
KwanYoung Moon <pianoguys775@gmail.com>
2022-11-15 23:03:55 +0900

    LinkedList Queue 구현 - poll() : 가장 앞 위치에 있는 요소인 head 요소를 삭제 - remove()와 poll()의 차이점 : 삭제할 요소가 없을 경우 remove()는 NoSuchElementException() 예외를 던지고, poll()의 경우는 null 반환

--- src/queue/LinkedListQueue.java
+++ src/queue/LinkedListQueue.java
@@ -2,6 +2,8 @@ package queue;
 
 import Interface_form.Queue;
 
+import java.util.NoSuchElementException;
+
 public class LinkedListQueue<E> implements Queue<E> {
 
     private Node<E> head;   // 큐에서 가장 앞에 있는 노드 객체를 가리키는 변수
@@ -33,4 +35,40 @@ public class LinkedListQueue<E> implements Queue<E> {
 
         return true;
     }
+
+    @Override
+    public E poll() {
+        // 삭제할 요소가 없을 경우 null 반환
+        if (size == 0) {
+            return null;
+        }
+
+        // 삭제될 요소의 데이터를 반환하기 위한 임시 변수
+        E element = head.data;
+
+        // head 노드의 다음 노드
+        Node<E> nextNode = head.next;
+
+        // head의 모든 데이터들을 삭제
+        head.data = null;
+        head.next = null;
+
+        // head가 가리키는 노드를, 삭제된 head 노드의 다음노드를 가리키도록 변경
+        head = nextNode;
+        size--;
+
+        return element;
+    }
+
+    public E remove() {
+
+        E element = poll();
+
+        if (element == null) {
+            throw new NoSuchElementException();
+        }
+
+        return element;
+    }
+
 }
