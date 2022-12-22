6cfc09aa0a4d8fc8ca65699a65e069bdfbf888fa
KwanYoung Moon <pianoguys775@gmail.com>
2022-12-22 23:58:15 +0900

    백준 2845 (파티가 끝나고 난 뒤, 수학)

---
+++ src/baekjoon/Number2845.java
@@ -0,0 +1,26 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Number2845 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int l = Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+        int num = l * p;
+
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while (st.hasMoreTokens()) {
+            int n = Integer.parseInt(st.nextToken());
+            sb.append(n - num + " ");
+        }
+
+        System.out.print(sb);
+    }
+}
