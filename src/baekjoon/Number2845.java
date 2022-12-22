513d27f5aa9dffd213e360212fe4fb7c177dca6a
KwanYoung Moon <pianoguys775@gmail.com>
2022-12-23 00:10:15 +0900

    백준 2845 (파티가 끝나고 난 뒤, 수학) - 코멘트 및 주석 추가 - 간단히 실제 사람 수와 기사에서의 사람 수의 차이만 구하면 된다.

--- src/baekjoon/Number2845.java
+++ src/baekjoon/Number2845.java
@@ -10,15 +10,15 @@ public class Number2845 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int l = Integer.parseInt(st.nextToken());
-        int p = Integer.parseInt(st.nextToken());
-        int num = l * p;
+        int L = Integer.parseInt(st.nextToken());   // 1m^2당 사람의 수
+        int P = Integer.parseInt(st.nextToken());   // 파티장 넓이
+        int num = L * P;    // 파티장에 있던 사람의 수
 
         st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         while (st.hasMoreTokens()) {
-            int n = Integer.parseInt(st.nextToken());
-            sb.append(n - num + " ");
+            int n = Integer.parseInt(st.nextToken());   // 기사에 실려있는 참가자의 수
+            sb.append(n - num).append(" ");   // 기사와 실제 사람 수의 차이
         }
 
         System.out.print(sb);
