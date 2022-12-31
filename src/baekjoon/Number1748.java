da25c8bb0780cb65cadecc63827c19ad40e88736
KwanYoung Moon <pianoguys775@gmail.com>
2022-12-31 17:08:09 +0900

    백준 1748 (수 이어 쓰기1, 수학) - N이 몇의 자리까지 있는지 부터 확인 (length) - 예를 들어 백의 자리까지 있다고 한다면(ex.103), 백의 자리는 세자리수이고 세자리수에 해당하는 개수는 103-99=4 가 된다.  -- 즉, 세자리수에 해당하는 개수(4) * 백의 자리 (3) = 12 로 세자리수에 해당하는 수의 총 개수를 알 수 있다.  -- 두자리수 개수는 99-9=90 만큼이 되고, 90*2=180 이 두자리수에 해당하는 수의 총 개수가 된다. - 예를 보면 알 수 있듯이 각 자릿수가 몇 개인지 공식으로 만들면, 새로운 수의 자릿수를 구할 수 있다.

--- src/baekjoon/Number1748.java
+++ src/baekjoon/Number1748.java
@@ -6,14 +6,28 @@ public class Number1748 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
+        String N = sc.next();
 
-        StringBuilder sb = new StringBuilder();
+        int length = N.length();
 
-        for (int i=1; i<=N; i++) {
-            sb.append(i);
+        if (Integer.parseInt(N) < 10) { // 1~9 사이의 수는 자릿수가 자신의 수와 같다.
+            System.out.println(N);
+            return;
         }
 
-        System.out.println(sb.length());
+        // ex. N이 102라면 3자릿수에 해당하는 수의 개수는 102-99=3 이다. // 100, 101, 102
+        // 102는 length가 3이고 즉, 3*3=9 만큼이 3자릿수에 해당하는 것이다.
+        int front = (Integer.parseInt(N) - Integer.parseInt("9".repeat(length - 1))) * length;  // ex. (1200-999) * 4 = 201*4
+
+        int num = 0;
+
+        while (length-2 >= 0) {
+            // ex. N=102 -> front 변수에 3자릿수의 개수는 구했고, 2자릿수 부터는 99-9=90 만큼이 2자릿수이므로 90*2=180이 된다.
+            num += Integer.parseInt("9" + "0".repeat(length-2)) * (length-1);
+
+            length = length - 1;
+        }
+
+        System.out.println(front + num);
     }
 }
