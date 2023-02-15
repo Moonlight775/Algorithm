69f69057bf93c379e2b1927f5d08c535412809b9
KwanYoung Moon <pianoguys775@gmail.com>
2023-02-15 19:25:26 +0900

    백준 1063 (킹, 구현) - 문제 자체를 잘 못 이해하여 전체 수정 - 항상 돌과 킹이 같이 움직이는 것이 아닌 킹만 움직이고 돌과 같은 위치가 되면 돌도 같은 움직임으로 이동 - 예외를 구하는 조건을 변경  -- 예외사항을 일일히 적어 불필요한 낭비발생  -- 체스판 밖으로 나가는 상황을 보는게 아닌 체스판 안에 있는지만 확인하여 간단하게 수정 - 변수를 int로 설정하여 문제 발생  -- char를 int로 변경하며 불필요한 로직과 낭비 발생  -- char[] 배열로 받아 해결 - 움직임에 따른 변화 메서드 수정  -- char로 움직임을 판단하려다 불필요하고 반복적인 로직 발생  -- String으로 변경하여 수정
    - 잘못된 문제 파악, 잘못된 변수 설정과 불필요한 로직등으로 인한 실패 경험

--- src/baekjoon/Number1063.java
+++ src/baekjoon/Number1063.java
@@ -3,102 +3,67 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Number1063 {
-
-    private static int king_row;    // 킹의 행 위치
-    private static int king_col;    // 킹의 열 위치
-    private static int rock_row;    // 돌의 행 위치
-    private static int rock_col;    // 돌의 열 위치
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        String king = st.nextToken();   // 킹의 위치
-        String rock = st.nextToken();   // 돌의 위치
+        char[] king = st.nextToken().toCharArray(); // 킹의 위치
+        char[] rock = st.nextToken().toCharArray(); // 돌의 위치
         int N = Integer.parseInt(st.nextToken());   // 움직이는 횟수
 
-        king_row = Math.abs(king.charAt(1)-'0'-8);   // 배열은 0부터 시작하므로 가장 아래가 0으로 되기 위해서는 -8을 해준다.
-        king_col = king.charAt(0)-65;   // 문자를 숫자로 변경, A -> 0
-        rock_row = Math.abs(rock.charAt(1)-'0'-8);  // 배열은 0부터 시작하므로 가장 아래가 0으로 되기 위해서는 -8을 해준다.
-        rock_col = rock.charAt(0)-65;   // 문자를 숫자로 변경, H -> 7
-
         while (N-- > 0) {
-            String move = br.readLine();
+            String move = br.readLine();    // 움직임
 
-            if (checkMove(move)) {
-                moving(move);
+            // 움직인 후 체스판 밖으로 나가는 경우 이동을 건너뛰어야 하므로 나가지 않을 때만 값을 변화하기 위해 temp 변수 사용
+            char[] temp_king = Arrays.copyOf(king, king.length);
+            char[] temp_rock = Arrays.copyOf(rock, rock.length);
+
+            moving(move, temp_king);    // 움직임에 따른 변화
+            if (check(temp_king)) continue; // 체스판 밖으로 나가는 경우 이번 움직임은 continue로 넘어가준다.
+
+            if (Arrays.equals(temp_king, temp_rock)) {  // 킹이 돌의 위치로 이동하는 경우 돌도 같은 움직임으로 이동
+                moving(move, temp_rock);
+                if (check(temp_rock)) continue;
             }
+
+            // 체스판 내에서 움직였다면 변수에 저장
+            king = temp_king;
+            rock = temp_rock;
         }
 
-        System.out.print((char)(king_col+65));
-        System.out.println(Math.abs(king_row-8));
-        System.out.print((char)(rock_col+65));
-        System.out.println(Math.abs(rock_row-8));
+        System.out.println(king);
+        System.out.println(rock);
     }
 
-    // 행동에 따라 이동
-    // T와 B가 헷갈릴 수 있는데, 문제와는 다르게 8의 위치를 0으로, 1의 위치를 7로 가정하여 행동
-    // 그렇기 때문에 -1을 해주면 위로, +1을 해주면 아래로 간다.
-    private static void moving(String move) {
-        switch (move.charAt(0)) {
-            case 'R' :
-                king_col++;
-                rock_col++;
-                break;
-            case 'L' :
-                king_col--;
-                rock_col--;
-                break;
-            case 'T' :
-                king_row--;
-                rock_row--;
-                break;
-            case 'B' :
-                king_row++;
-                rock_row++;
-                break;
-        }
-
-        if (move.length() > 1) {
-            switch (move.charAt(1)) {
-                case 'T' :
-                    king_row--;
-                    rock_row--;
-                    break;
-                case 'B' :
-                    king_row++;
-                    rock_row++;
-                    break;
-            }
-        }
+    // 체스판 범위 안에 있는지 확인하는 메서드
+    private static boolean check(char[] pices) {
+        return pices[0] < 'A' || pices[0] > 'H' || pices[1] < '1' || pices[1] > '8';
     }
 
-    // 입력대로 이동할 때 킹이나 돌이 체스판 밖으로 나가는지 확인
-    // 밖으로 나갈 경우 행동 X
-    private static boolean checkMove(String move) {
-        switch (move.charAt(0)) {
-            case 'R' : if (king_col == 7 || rock_col == 7) return false;
+    // 움직임에 따른 변화 메서드
+    private static void moving(String move, char[] pices) {
+        switch (move) {
+            case "R" : pices[0]++;
                 break;
-            case 'L' : if (king_col == 0 || rock_col == 0) return false;
+            case "L" : pices[0]--;
                 break;
-            case 'T' : if (king_row == 0 || rock_row == 0 ) return false;
+            case "B" : pices[1]--;
                 break;
-            case 'B' : if (king_row == 7 || rock_row == 7) return false;
+            case "T" : pices[1]++;
+                break;
+            case "RT" : pices[0]++; pices[1]++;
+                break;
+            case "LT" : pices[0]--; pices[1]++;
+                break;
+            case "RB" : pices[0]++; pices[1]--;
+                break;
+            case "LB" : pices[0]--; pices[1]--;
                 break;
         }
-
-        if (move.length() > 1) {
-            switch (move.charAt(1)) {
-                case 'T' : if (king_row == 0 || rock_row == 0 ) return false;
-                    break;
-                case 'B' : if (king_row == 7 || rock_row == 7) return false;
-                    break;
-            }
-        }
-
-        return true;
     }
 }
