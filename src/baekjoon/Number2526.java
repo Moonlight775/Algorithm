cb2f0c5e53c5dcb9646a09f5fe2d4cbe05265f2e
KwanYoung Moon <pianoguys775@gmail.com>
2023-03-01 23:26:22 +0900

    백준 2526 (싸이클, 구현) - 기존 count를 세는 방식이 잘 못 되어 출력을 for문 안에서list.size() - i 로 변경 - num 변수는 이중으로 처리하고 있어서 바로 rest 변수에 변경이 적용되도록 변경

--- src/baekjoon/Number2526.java
+++ src/baekjoon/Number2526.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.StringTokenizer;
 
 public class Number2526 {
@@ -15,27 +14,20 @@ public class Number2526 {
         int N = Integer.parseInt(st.nextToken());
         int P = Integer.parseInt(st.nextToken());
         int rest = N;   // 다음 값을 구하기 위한 나머지
-        int count = 0;  // 서로 다른 수의 개수
 
         ArrayList<Integer> list = new ArrayList<>();    // 서로 다른 수를 담을 리스트
-        list.add(N);
 
         while (true) {
-            int num = (rest * N) % P;
+            rest = (rest * N) % P;
 
-            list.add(num);
-
-            for (int i=0; i<list.size()-1; i++) {
-                for (int j=i+1; j<list.size(); j++) {
-                    if (Objects.equals(list.get(i), list.get(j))) {   // 같은 수가 나오면 종료
-                        System.out.println(count);
-                        return;
-                    }
+            for (int i=0; i<list.size(); i++) {
+                if (rest == list.get(i)) {
+                    System.out.println(list.size() - i);
+                    return;
                 }
             }
 
-            count++;
-            rest = num;
+            list.add(rest);
         }
     }
 }
