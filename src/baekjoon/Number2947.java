3d42ed5af69028041ee6f562c8bacc03cc2ed58b
KwanYoung Moon <pianoguys775@gmail.com>
2023-03-05 20:50:48 +0900

    백준 2947 (나무 조각, 구현) - flag를 사용해 순서가 정해지면 while문 탈출하도록 변경

--- src/baekjoon/Number2947.java
+++ src/baekjoon/Number2947.java
@@ -3,7 +3,6 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Number2947 {
@@ -12,36 +11,31 @@ public class Number2947 {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int[] arr = new int[5];
-        int[] compare = {1, 2, 3, 4, 5};
 
         // 나무 조각 순서 초기화
         for (int i = 0; i < 5; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        boolean flag = true;
         StringBuilder sb = new StringBuilder();
-        while (true) {
+        while (flag) {
+            flag = false;
+
             // 조건에 맞게 순서 변경
             for (int i = 0; i < 4; i++) {
                 if (arr[i] > arr[i + 1]) {
+                    flag = true;
                     int temp = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i+1] = temp;
-                    break;
+
+                    for (int val : arr) {
+                        sb.append(val).append(" ");
+                    }
+                    sb.append('\n');
                 }
             }
-
-            // 출력
-            for (int val : arr) {
-                sb.append(val).append(" ");
-            }
-
-            // 1, 2, 3, 4, 5 순서에 맞다면 종료
-            if (Arrays.equals(arr, compare)) {
-                break;
-            }
-
-            sb.append('\n');
         }
 
         System.out.println(sb);
