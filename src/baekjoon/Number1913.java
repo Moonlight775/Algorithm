15d7c342a814857f68cc5dc33c84cc7a2f7a8a89
KwanYoung Moon <pianoguys775@gmail.com>
2023-03-09 07:08:37 +0900

    백준 1913 (달팽이, 구현) - 단순하게 이중 for문으로 구현 - 예제는 출력되나 틀림처리 되어 수정필요

---
+++ src/baekjoon/Number1913.java
@@ -0,0 +1,63 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number1913 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int num = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][N];
+
+        int row = N / 2;
+        int col = N / 2;
+        int number = 1;
+        arr[col][row] = 1;
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if (i % 2 == 1) {
+                    arr[--col][row] = ++number;
+                } else {
+                    arr[++col][row] = ++number;
+                }
+            }
+
+            for (int j = 0; j < i; j++) {
+                if (i % 2 == 1) {
+                    arr[col][++row] = ++number;
+                } else {
+                    arr[col][--row] = ++number;
+                }
+            }
+        }
+
+        for (int i = N - 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                arr[--col][row] = ++number;
+            }
+        }
+
+        int index_col = 0;
+        int index_row = 0;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (arr[i][j] == num) {
+                    index_col = i;
+                    index_row = j;
+                }
+                sb.append(arr[i][j]);
+            }
+            sb.append('\n');
+        }
+
+        System.out.print(sb);
+        System.out.println((index_col+1) + " " + (index_row+1));
+    }
+}
