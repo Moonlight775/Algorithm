31d3e7f8658f297ed5760e80bc28ff9d375e6882
KwanYoung Moon <pianoguys775@gmail.com>
2023-03-22 22:49:23 +0900

    백준 15894 (수학은 체육과목 입니다, 수학) - 도형의 둘레는 정사각형 개수 X 4 와 같다. - 정사각형 개수가 10^9까지이므로 long 사용

---
+++ src/baekjoon/Number15894.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Number15894 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+
+        System.out.println(n * 4);
+    }
+}
