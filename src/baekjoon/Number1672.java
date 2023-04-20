9da251d761aae608ca3f188d9c7913344b1d0888
KwanYoung Moon <pianoguys775@gmail.com>
2023-04-21 05:38:56 +0900

    백준 1672 (DNA 해독, 문자열) - 암호표를 하드코딩으로 초기화 - 염기 서열의 끝에 두 문자를 행과 열로 변경 - substring()을 사용해 끝 두 문자를 자르고 암호표에 맞는 문자를 추가 - 메모리 초과로 수정 필요

---
+++ src/baekjoon/Number1672.java
@@ -0,0 +1,47 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number1672 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        char[][] chart = new char[4][4];
+
+        // 암호표 초기화
+        chart[0][0] = chart[0][2] = chart[1][3] = chart[2][0] = chart[3][1] = 'A';
+        chart[0][1] = chart[1][0] = chart[2][2] = 'C';
+        chart[0][3] = chart[1][1] = chart[2][3] = chart[3][0] = chart[3][2] = 'G';
+        chart[1][2] = chart[2][1] = chart[3][3] = 'T';
+
+        String str = br.readLine();
+        int length = str.length();
+
+        for (int i = length - 1; i > 0; i--) {
+            int row = getNum(str.charAt(i - 1));    // 행
+            int col = getNum(str.charAt(i));        // 열
+
+            str = str.substring(0, i-1);
+            str += chart[row][col];
+        }
+
+        System.out.println(str);
+    }
+
+    // 행과 열의 위치를 알기 위한 메소드
+    public static int getNum(char ch) {
+        if (ch == 'A') {
+            return 0;
+        } else if (ch == 'G') {
+            return 1;
+        } else if (ch == 'C') {
+            return 2;
+        } else {
+            return 3;
+        }
+    }
+}
