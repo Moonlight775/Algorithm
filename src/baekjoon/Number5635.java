0f0371bea49e209d620b85ab4ef7a34441e778f8
KwanYoung Moon <pianoguys775@gmail.com>
2023-04-20 21:31:56 +0900

    백준 5635 (생일, 문자열, 정렬) - Comparator 람다식으로 변경

--- src/baekjoon/Number5635.java
+++ src/baekjoon/Number5635.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Number5635 {
@@ -24,20 +23,17 @@ public class Number5635 {
         }
 
         // 연, 월, 일 순으로 정렬
-        Arrays.sort(student, new Comparator<String[]>() {
-            @Override
-            public int compare(String[] o1, String[] o2) {
-                if (o1[3].equals(o2[3])) {
-                    if (o1[2].equals(o2[2])) {
-                        return Integer.parseInt(o1[1]) - Integer.parseInt(o2[1]);
-                    }
-                    else {
-                        return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);
-                    }
+        Arrays.sort(student, (o1, o2) -> {
+            if (o1[3].equals(o2[3])) {
+                if (o1[2].equals(o2[2])) {
+                    return Integer.parseInt(o1[1]) - Integer.parseInt(o2[1]);
                 }
                 else {
-                    return Integer.parseInt(o1[3]) - Integer.parseInt(o2[3]);
+                    return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);
                 }
+            }
+            else {
+                return Integer.parseInt(o1[3]) - Integer.parseInt(o2[3]);
             }
         });
 
