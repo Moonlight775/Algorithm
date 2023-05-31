0abe8ba80440bdce38a5b08d61dbf51bbad15487
KwanYoung Moon <pianoguys775@gmail.com>
2023-05-31 16:12:06 +0900

    링 버퍼로 큐를 구현하는 프로그램 생성 - 인큐 메서드 enque - 큐에 데이터를 인큐하는 메서드, rear 값이 max와 같아지는 것을 방지하도록 rear 값을 1만큼 증가했을 때 큐의 최대 용량의 값인 max와 같아질 경우 rear를 배열의 처음인 0으로 변경 - 디큐 메서드 deque - 큐에서 데이터를 디큐하고 그 값을 반환하는 메서드, 1만큼 증가한 front 값이 큐의 용량인 max와 같아지면 front 값을 배열의 처음인 0으로 변경

--- src/chap04/IntQueue.java
+++ src/chap04/IntQueue.java
@@ -28,4 +28,38 @@ public class IntQueue {
             max = 0;
         }
     }
+
+    // 큐에 데이터를 인큐
+    public int enque(int x) throws OverflowIntQueueException {
+        if (num >= max) {
+            throw new OverflowIntQueueException();      // 큐가 가득 참
+        }
+
+        que[rear++] = x;
+        num++;
+
+        if (rear == max) {
+            rear = 0;
+        }
+
+        return x;
+    }
+
+    // 큐에서 데이터를 디큐
+    public int deque() throws EmptyIntQueueException {
+        if (num <= 0) {
+            throw new EmptyIntQueueException();     // 큐가 비어 있음
+        }
+
+        int x = que[front++];
+        num--;
+
+        if (front == max) {
+            front = 0;
+        }
+
+        return x;
+    }
+
+
 }
