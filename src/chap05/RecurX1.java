8790ea2aa5bb71634cdd5f87c2706bdb887c9273
KwanYoung Moon <pianoguys775@gmail.com>
2023-06-28 18:53:58 +0900

    재귀 알고리즘의 비재귀적 표현 - 꼬리 재귀의 제거

---
+++ src/chap05/RecurX1.java
@@ -0,0 +1,23 @@
+package chap05;
+
+import java.util.Scanner;
+
+public class RecurX1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("정수를 입력하세요. : ");
+        int x = sc.nextInt();
+
+        recur(x);
+    }
+
+    // 꼬리 재귀를 제거
+    static void recur(int n) {
+        if (n > 0) {
+            recur(n-1);
+            System.out.println(n);
+            n = n - 2;
+        }
+    }
+}
