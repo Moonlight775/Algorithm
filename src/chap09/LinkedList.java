cbbbf69e5075b631d361939cec65298daad4d936
KwanYoung Moon <pianoguys775@gmail.com>
2023-09-12 23:08:48 +0900

    포인터로 연결 리스트 생성 - search(E obj, Comparator<? super E> c) : 어떤 조건을 만족하는 노드를 검색 - addFirst(E obj) : 머리에 노드를 삽입 - addLast(E obj) : 꼬리에 노드를 삽입

--- src/chap09/LinkedList.java
+++ src/chap09/LinkedList.java
@@ -1,5 +1,7 @@
 package chap09;
 
+import java.util.Comparator;
+
 public class LinkedList<E> {
     // 노드
     class Node<E> {
@@ -19,5 +21,38 @@ public class LinkedList<E> {
     // 생성자
     public LinkedList() {
         head = crnt = null;
+    }
+
+    // 노드 검색
+    public E search(E obj, Comparator<? super E> c) {
+        Node<E> ptr = head;     // 현재 스캔 중인 노드
+
+        while (ptr != null) {
+            if (c.compare(obj, ptr.data) == 0) {    // 검색 성공
+                crnt = ptr;
+                return ptr.data;
+            }
+            ptr = ptr.next;     // 다음 노드를 선택
+        }
+        return null;    // 검색 실패
+    }
+
+    // 머리에 노드 삽입
+    public void addFirst(E obj) {
+        Node<E> ptr = head;     // 삽입 전의 머리 노드
+        head = crnt = new Node<E>(obj, ptr);
+    }
+
+    // 꼬리에 노드 삽입
+    public void addLast(E obj) {
+        if (head == null) {     // 리스트가 비어 있으면
+            addFirst(obj);      // 머리에 삽입
+        } else {
+            Node<E> ptr = head;
+            while (ptr.next != null) {
+                ptr = ptr.next;
+            }
+            ptr.next = crnt = new Node<E>(obj, null);
+        }
     }
 }
