a1367156e39c2b101edda226451d791403844cd3
KwanYoung Moon <pianoguys775@gmail.com>
2023-09-16 21:50:26 +0900

    연결 리스트 - comparator c 메서드를 사용해 서로 같은 노드를 찾아 모두 삭제하는 메서드 생성

--- src/chap09/LinkedList.java
+++ src/chap09/LinkedList.java
@@ -140,4 +140,35 @@ public class LinkedList<E> {
             ptr = ptr.next;
         }
     }
+
+    // comparator c에 의해 서로 같다고 보는 노드를 모두 삭제
+    public void purge(Comparator<? super E> c) {
+        Node<E> ptr = head;
+
+        while (ptr != null) {
+            int count =0;
+            Node<E> ptr2 = ptr;
+            Node<E> pre = ptr;
+
+            while (pre.next != null) {
+                ptr2 = pre.next;
+                if (c.compare(ptr.data, ptr2.data) == 0) {
+                    pre.next = ptr2.next;
+                    count++;
+                } else {
+                    pre = ptr2;
+                }
+            }
+
+            if (count == 0) {
+                ptr = ptr.next;
+            } else {
+                Node<E> temp = ptr;
+                remove(ptr);
+                ptr = temp.next;
+            }
+        }
+
+        crnt = head;
+    }
 }
