c1b52d9eb10456504b7ec7ce75da75499198d819
KwanYoung Moon <pianoguys775@gmail.com>
2023-10-07 22:51:23 +0900

    이진검색트리 프로그램 구현 - remove(K key): 키 값이 key인 노드를 삭제하는 메서드  -- 자식 노드가 없는 노드를 삭제하는 경우, 자식 노드가 1개인 노드를 삭제하는 경우, 자식 노드가 2개인 노드를 삭제하는 경우로 세 가지 서로 다른 상황에 따라 알맞은 처리를 설정

--- src/chap10/BinTree.java
+++ src/chap10/BinTree.java
@@ -103,4 +103,72 @@ public class BinTree<K, V> {
             addNode(root, key, data);
         }
     }
+
+    // 키 값이 key인 노드를 삭제
+    public boolean remove(K key) {
+        Node<K,V> p = root;             // 스캔 중인 노드
+        Node<K,V> parent = null;        // 스캔 중인 노드의 부모 노드
+        boolean isLeftChild = true;     // p는 부모의 왼쪽 자식 노드인가?
+
+        while (true) {
+            if (p == null) {        // 더 이상 진행하지 않으면
+                return false;       // 그 키 값은 없다.
+            }
+
+            int cond = comp(key, p.getKey());   // key와 노드 p의 키 값을 비교
+
+            if (cond == 0) {        // 같으면
+                break;              // 검색 성공
+            } else {
+                parent = p;         // 가지로 내려가기 전에 부모를 설정
+                if (cond < 0) {     // key 쪽이 작으면
+                    isLeftChild = true;     // 왼쪽 자식으로 내려감
+                    p = p.left;     // 왼쪽 서브 트리에서 검색
+                } else {
+                    isLeftChild = false;    // 오른쪽 자식으로 내려감
+                    p = p.right;    // 오른쪽 서브 트리에서 검색
+                }
+            }
+        }
+
+        if (p.left == null) {   // p에는 왼쪽 자식이 없음
+            if (p == root) {
+                root = p.right;
+            } else if (isLeftChild) {
+                parent.left = p.right;  // 부모의 왼쪽 포인터가 오른쪽 자식을 가리킴
+            } else {
+                parent.right = p.right; // 부모의 오른쪽 포인터가 오른쪽 자식을 가리킴
+            }
+
+        } else if (p.right == null) {   // p에는 오른쪽 자식이 없음
+            if (p == root) {
+                root = p.left;
+            } else if (isLeftChild) {
+                parent.left = p.left;   // 부모의 왼쪽 포인터가 왼쪽 자식을 가리킴
+            } else {
+                parent.right = p.left;  // 부모의 오른쪽 포인터가 왼쪽 자식을 가리킴
+            }
+
+        } else {
+            parent = p;
+            Node<K,V> left = p.left;    // 서브 트리 가운데 가장 큰 노드
+            isLeftChild = true;
+
+            while (left.right != null) {    // 가장 큰 노드 left를 검색
+                parent = left;
+                left = left.right;
+                isLeftChild = false;
+            }
+
+            p.key = left.key;       // left의 키 값을 p로 옮김
+            p.data = left.data;     // left의 데이터를 p로 옮김
+            if (isLeftChild) {
+                parent.left = left.left;    // left를 삭제
+            } else {
+                parent.right = left.left;   // left를 삭제
+            }
+        }
+
+        return true;
+    }
 }
