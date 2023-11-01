53732b6232425db6c4b7414c78b6c650d667bef9
KwanYoung Moon <pianoguys775@gmail.com>
2023-11-01 23:00:09 +0900

    클래스 ChainHash<K,V>를 사용한 프로그램 생성 - 메뉴 열거형 생성 - 메뉴 선택 메서드 생성 - main 메서드 생성

--- src/chqp11/ChainHashTester.java
+++ src/chqp11/ChainHashTester.java
@@ -39,4 +39,82 @@ public class ChainHashTester {
         }
     }
 
+    // 메뉴 열거형
+    enum Menu {
+        ADD(      "추가"),
+        REMOVE(   "삭제"),
+        SEARCH(   "검색"),
+        DUMP(     "표시"),
+        TERMINATE("종료");
+
+        private final String message;   // 출력할 문자열
+
+        static Menu MenuAt(int idx) {   // 서수가 idx인 열거를 반환
+            for (Menu m : Menu.values()) {
+                if (m.ordinal() == idx) {
+                    return m;
+                }
+            }
+            return null;
+        }
+
+        Menu(String string) {       // 생성자
+            message = string;
+        }
+
+        String getMessage() {       // 출력할 문자열을 반환
+            return message;
+        }
+    }
+
+    // 메뉴 선택
+    static Menu SelectMenu() {
+        int key;
+        do {
+            for (Menu m : Menu.values()) {
+                System.out.printf("(%d) %s ", m.ordinal(), m.getMessage());
+            }
+            System.out.print(" : ");
+            key = stdIn.nextInt();
+        } while (Menu.ADD.ordinal() > key || key > Menu.TERMINATE.ordinal());
+
+        return Menu.MenuAt(key);
+    }
+
+    public static void main(String[] args) {
+        Menu menu;      // 메뉴
+        Data data;      // 추가용 데이터 참조
+        Data temp = new Data();     // 입력용 데이터
+
+        ChainHash<Integer, Data> hash = new ChainHash<>(13);
+
+        do {
+            switch (menu = SelectMenu()) {
+                case ADD:       // 추가
+                    data = new Data();
+                    data.scanData("추가", Data.NO | Data.NAME);
+                    hash.add(data.keyCode(), data);
+                    break;
+
+                case REMOVE:    // 삭제
+                    temp.scanData("삭제", Data.NO);
+                    hash.remove(temp.keyCode());
+                    break;
+
+                case SEARCH:    // 검색
+                    temp.scanData("검색", Data.NO);
+                    Data t = hash.search(temp.keyCode());
+                    if (t != null) {
+                        System.out.println("그 키를 갖는 데이터는 " + t + "입니다.");
+                    } else {
+                        System.out.println("그 데이터가 없습니다.");
+                    }
+                    break;
+
+                case DUMP:      // 출력
+                    hash.dump();
+                    break;
+            }
+        } while (menu != Menu.TERMINATE);
+    }
 }
