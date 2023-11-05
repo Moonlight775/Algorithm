44029b8cd342ce422af00b7f848f1cf2fa29adb7
KwanYoung Moon <pianoguys775@gmail.com>
2023-11-05 11:37:15 +0900

    오픈 주소법에 의한 해시 구현 - 생성자 생성 - hashValue(Object key): 해시 값을 구하는 메서드 - rehashValue(int hash): 재해시 값을 구하는 메서드 - searchNode(K key): 키 값 key를 갖는 버킷을 검색하는 메서드 - search(K key): 키 값 key를 갖는 요소를 검색하는 메서드(데이터를 반환)

--- src/chqp11/OpenHash.java
+++ src/chqp11/OpenHash.java
@@ -44,4 +44,56 @@ public class OpenHash<K,V> {
         }
     }
 
+    private int size;               // 해시 테이블의 크기
+    private Bucket<K,V>[] table;    // 해시 테이블
+
+    // 생성자
+    public OpenHash(int size) {
+        try {
+            table = new Bucket[size];
+            for (int i = 0; i < size; i++) {
+                table[i] = new Bucket<K, V>();
+            }
+            this.size = size;
+        } catch (OutOfMemoryError e) {      // 테이블을 생성할 수 없음
+            this.size = 0;
+        }
+    }
+
+    // 해시 값을 구함
+    public int hashValue(Object key) {
+        return key.hashCode() % size;
+    }
+
+    // 재해시 값을 구함
+    public int rehashValue(int hash) {
+        return (hash + 1) % size;
+    }
+
+    // 키 값 key를 갖는 버킷의 검색
+    private Bucket<K, V> searchNode(K key) {
+        int hash = hashValue(key);      // 검색할 데이터의 해시 값
+        Bucket<K,V> p = table[hash];    // 선택 버킷
+
+        for (int i = 0; p.stat != Status.EMPTY && i < size; i++) {
+            if (p.stat == Status.OCCUPIED && p.getKey().equals(key)) {
+                return p;
+            }
+            hash = rehashValue(hash);   // 재해시
+            p = table[hash];
+        }
+        return null;
+    }
+
+    // 키 값 key를 갖는 요소의 검색 (데이터를 반환)
+    public V search(K key) {
+        Bucket<K, V> p = searchNode(key);
+
+        if (p != null) {
+            return p.getValue();
+        } else {
+            return null;
+        }
+    }
+
 }
