1f560d581b2f9636b21f497762b276342d559ac2
KwanYoung Moon <pianoguys775@gmail.com>
2023-12-23 04:31:06 +0900

    백준 11660 (구간 합 구하기 5, 누적 합) 수정 - 원본 배열을 토대로 구간 합 배열을 생성  -- sum[i][j]의 값을 채우는 구간 합 공식은 sum[i][j] = sum[i-1]sum[j] + sum[i][j-1] - sum[i-1][j-1] + arr[i][j] 이다. - 주어지는 x1, y1, x2, y2에 대한 답을 구간 합으로 구하는 공식은 sum[x2][y2] - sum[x1-1][y2] - sum[x2][y-1] + sum[x1-1][y1-1] 이다.

--- src/baekjoon/Number11660.java
+++ src/baekjoon/Number11660.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 
 public class Number11660 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+/*        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());   // 표의 크기
@@ -41,5 +41,45 @@ public class Number11660 {
         }
 
         System.out.println(sb);
+ */
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());   // 2차원 배열의 크기
+        int M = Integer.parseInt(st.nextToken());   // 합을 구해야 하는 횟수
+
+        int[][] arr = new int[N + 1][N + 1];    // 원본 배열
+
+        // 원본 배열 초기화
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] sum = new int[N + 1][N + 1];    // 구간 합 배열
+
+        // 구간 합 배열 초기화
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + arr[i][j];
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            // 구간 합을 구할 좌표
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int result = sum[x2][y2] - sum[x1 - 1][y2] - sum[x2][y1 - 1] + sum[x1 - 1][y1 - 1];
+
+            System.out.println(result);
+        }
     }
 }
