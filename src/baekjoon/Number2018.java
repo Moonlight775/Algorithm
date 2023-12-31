d1afc622175a4a2c4bdf473a83f6be8cf2e556fb
KwanYoung Moon <pianoguys775@gmail.com>
2023-12-31 23:47:03 +0900

    백준 2018 (수들의 합 5, 투 포인터) - 결과 변수 count를 1로 초기화하는 이유는 N값 그 자체의 숫자인 경우의 수를 넣고 초기화했기 때문이다. - 투 포인터 이동 원칙을 활용해 배열의 끝까지 탐색하면서 합이 N이 될 경우의 수를 구한다.  -- 투 포인터 이동 원칙 :  sum > N: sum -= start_index; satrt_index++;  sum < N: end_index++; sum += end_index;  sum == N: end_index++; sum += end_index; count++; - end_index가 N이 될 때까지 반복하고, 포인터가 이동할 때마다 현재의 총합과 N을 비교해 값이 같으면 count를 1만큼 증가시킨다.

--- src/baekjoon/Number2018.java
+++ src/baekjoon/Number2018.java
@@ -9,7 +9,28 @@ public class Number2018 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());    // 자연수
-        int count = 0;  // 연속된 자연수의 합의 개수
+        int count = 1;
+        int start_index = 1;
+        int end_index = 1;
+        int sum = 1;
+
+        while (end_index != N) {
+            if (sum == N) {         // 현재 연속 합이 N과 같은 경우
+                count++;
+                end_index++;
+                sum += end_index;
+            } else if (sum > N) {   // 현재 연속 합이 N보다 더 큰 경우
+                sum -= start_index;
+                start_index++;
+            } else {                // 현재 연속 합이 N보다 작은 경우
+                end_index++;
+                sum += end_index;
+            }
+        }
+
+        System.out.println(count);
+
+        /* int count = 0;  // 연속된 자연수의 합의 개수
 
         for (int i=1; i<=N/2; i++) {    // N/2 이상의 수 부터는 무조건 연속된 합은 N보다 커지므로 범위를 N/2로 지정
             int sum = i;
@@ -26,5 +47,6 @@ public class Number2018 {
         }
 
         System.out.println(count+1);    // 자기 자신의 수까지 포함해야하므로 +1
+        */
     }
 }
