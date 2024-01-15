0fdef4ef5216db5d4aca2221cbc593a721b34a88
KwanYoung Moon <pianoguys775@gmail.com>
2024-01-15 12:47:39 +0900

    백준 1874 (스택 수열, 스택) - 1부터 자연수를 증가시키면서 입력으로 주어진 숫자와 비교하여 증가시킨 자연수를 스택에 추가하거나 빼는 방식으로 푼다. - 스택 연산은 다음 2가지 방법으로 수행  -- 현재 수열 값 >= 자연수   현재 수열 값이 자연수보다 크거나 같을 때까지 자연수를 1씩 증가시키며 자연수를 스택에 push. 그리고 push가 끝나면 수열을 출력하기 위해 마지막 1회만 pop한다.   ex) 현재 수열 값이 4면 스택에는 1, 2, 3, 4를 push하고 마지막 1회만 pop하여 4를 출력한 뒤 조건문을 빠져나오고 자연수는 5가 된다.  -- 현재 수열 값 < 자연수   현재 수열 값보다 자연수가 크다면 pop으로 스택에 있는 값을 꺼낸다. 꺼낸 값이 현재 수열 값이거나 아닐 수 있다. 만약 아니라면 후입선출 원리에 따라 수열을 표현할 수 없으므로 "NO"를 출력한 후 문제를 종료하고, 현재 수열 값이라면 그대로 조건문을 빠져나온다.   ex) 위의 예를 이어 설명하면 자연수는 5, 현재 수열 값은 3이므로 스택에서 3을 꺼낸다. 현재 수열 값과 스택에서 꺼낸 값이 같으면 계속해서 스택 연산을 수행. 스택에는 1, 2가 남아있으며, 자연수는 5이다. - 연산이 가능하다면 출력 후 종료

--- src/baekjoon/Number1874.java
+++ src/baekjoon/Number1874.java
@@ -8,22 +8,59 @@ import java.util.Stack;
 public class Number1874 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
 
         int n = Integer.parseInt(br.readLine());    // 입력 받을 수의 개수
+        int[] arr = new int[n];
 
-        int num = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
 
-        while (n-- > 0) {   // n번 반복
+        StringBuffer sb = new StringBuffer();
+        int num = 1;    // 오름차순 수
+        boolean flag = true;
+
+        for (int i = 0; i < n; i++) {
+            int now = arr[i];    // 현재 수열의 수
+
+            // 현재 수열 값 >= 오름차순 자연수: 값이 같아질 때까지 push() 수행
+            if (now >= num) {
+                while (now >= num) {
+                    stack.push(num++);
+                    sb.append("+\n");
+                }
+                stack.pop();
+                sb.append("-\n");
+            }
+            // 현재 수열 값 < 오름차순 자연수: pop()을 수행해 수열 원소를 꺼낸다
+            else {
+                int pop_num = stack.pop();
+
+                // 스택의 가장 위의 수가 만들어야 하는 수열의 수보다 크면 수열을 출력할 수 없다
+                if (pop_num > now) {
+                    System.out.println("NO");
+                    flag = false;
+                    break;
+                } else {
+                    sb.append("-\n");
+                }
+            }
+        }
+
+        if (flag) {
+            System.out.println(sb);
+        }
+
+/*        while (n-- > 0) {   // n번 반복
             int sequence = Integer.parseInt(br.readLine());     // 입력된 정수
 
-            if (sequence > num) {   // 정수가 1일 경우도 push해주어야 하므로 num = 0으로 초기화
+            if (sequence > num) {   // 정수가 1일 경우도 push 해주어야 하므로 num = 0으로 초기화
                 for (int i=num+1; i<=sequence; i++) {   // 1부터 입력된 정수까지
                     stack.push(i);                      // 스택에 push 해준다.
                     sb.append('+').append('\n');
                 }
-                num = sequence;     // 입력된 정수까지 스택에 쌓였으므로 그 다음 수부터 처리하기 위해 num을 입력된 정수로 초기화
+                num = sequence;     // 입력된 정수까지 스택에 쌓였으므로 그 다음 수부터 처리하기 위해 num 을 입력된 정수로 초기화
             }
 
             if (stack.peek() == sequence) {     // 스택의 맨 위 원소가 입력된 정수와 같다면
@@ -36,5 +73,6 @@ public class Number1874 {
         }
 
         System.out.println(sb);
+ */
     }
 }
