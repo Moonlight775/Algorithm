9ddc7895e8af5b6d392622478252924eda94d955
KwanYoung Moon <pianoguys775@gmail.com>
2024-02-09 21:33:18 +0900

    Heap 구현 - isEmpty(): 현재 힙에 요소가 아무 것도 없는지 판별 - toArray(): 힙의 배열에 요소가 어떻게 배치되어 있는지 볼 수 있는지 확인

--- src/heap/Heap.java
+++ src/heap/Heap.java
@@ -1,5 +1,6 @@
 package heap;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.NoSuchElementException;
 
@@ -263,5 +264,13 @@ public class Heap<E> {
             throw new NoSuchElementException();
         }
         return (E) array[1];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public Object[] toArray() {
+        return Arrays.copyOf(array, size + 1);
     }
 }
