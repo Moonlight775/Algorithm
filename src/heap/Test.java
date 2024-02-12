7c23ea5b4d4b9d7b902f63a2ad8af5668064978e
KwanYoung Moon <pianoguys775@gmail.com>
2024-02-12 22:51:29 +0900

    Heap 테스트 - 사용자 정의 클래스일 경우 테스트 - heap1 은 기본 정렬을 지정하여 쓴 방식(Comparable)이고, heap2 는 Comparator를 사용하여 Comparable 과는 다른 정렬방식을 지정하여 생성할 때 인자로 넘겨 준다.

--- src/heap/Test.java
+++ src/heap/Test.java
@@ -1,28 +1,73 @@
 package heap;
 
-import java.util.Random;
+import java.util.Comparator;
 
 public class Test {
     public static void main(String[] args) {
-        Heap<Integer> heap = new Heap<>();
+        Heap<Student> heap1 = new Heap<>();
+        Heap<Student> heap2 = new Heap<>(comparator);
 
-        Random rnd = new Random();
+        heap1.add(new Student("김자바", 40));
+        heap2.add(new Student("김자바", 40));
 
-        for (int i = 0; i < 15; i++) {
-            heap.add(rnd.nextInt(100));
-        }
+        heap1.add(new Student("이씨프", 27));
+        heap2.add(new Student("이씨프", 27));
 
-        // 힙 내부 배열의 요소 상태
-        System.out.print("내부 배열 상태 : ");
-        for (Object val : heap.toArray()) {
-            System.out.print(val + " ");
+        heap1.add(new Student("조파이", 48));
+        heap2.add(new Student("조파이", 48));
+
+        heap1.add(new Student("김자바", 18));
+        heap2.add(new Student("김자바", 18));
+
+        heap1.add(new Student("상스윕", 32));
+        heap2.add(new Student("상스윕", 32));
+
+        heap1.add(new Student("양씨샵", 27));
+        heap2.add(new Student("양씨샵", 27));
+
+        System.out.println("[Heap 1] : 이름순(같을 경우 나이 오름차순)");
+        while (!heap1.isEmpty()) {
+            System.out.println(heap1.remove());
         }
         System.out.println();
 
-        // 힙이 비어있을 때까지 한 개씩 요소 뽑음
-        System.out.print("힙 요소 뽑기 : \t");
-        while (!heap.isEmpty()) {
-            System.out.print(heap.remove() + " ");
+        System.out.println("[Heap 2] : 나이 내림차순(같을 경우 이름순)");
+        while (!heap2.isEmpty()) {
+            System.out.println(heap2.remove());
+        }
+        System.out.println();
+    }
+
+    private static final Comparator<Student> comparator = (o1, o2) -> {
+        // 나이가 같다면 이름순
+        if (o1.age == o2.age) {
+            return o1.name.compareTo(o2.name);
+        }
+
+        return o2.age - o1.age; // 나이 내림차순
+    };
+
+    private static class Student implements Comparable<Student> {
+        String name;
+        int age;
+
+        public Student(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public int compareTo(Student o) {
+            // 이름이 같다면 나이순 (오름차순)
+            if (this.name.compareTo(o.name) == 0) {
+                return this.age - o.age;
+            }
+            // 이름순
+            return this.name.compareTo(o.name);
+        }
+
+        public String toString() {
+            return "이름 : " + name + "\t나이 : " + age;
         }
     }
 }
