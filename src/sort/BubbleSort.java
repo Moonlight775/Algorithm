1a6d1471ef58d05ca99e92059830eeaba6e60eb8
KwanYoung Moon <pianoguys775@gmail.com>
2024-02-21 05:40:58 +0900

    거품 정렬(Bubble Sort) 구현 - 시간복잡도 개선을 위한 수정 - swap이 발생하지 않는다면 이미 정렬된 데이터라는 의미이기 때문에 정렬을 종료 - Best Case의 경우 O(N)

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -9,12 +9,22 @@ public class BubbleSort {
     private static void bubble_sort(int[] a, int size) {
         // round는 배열 크기 - 1 만큼 진행됨
         for (int i = 1; i < size; i++) {
+
+            boolean swapped = false;
+
             // 각 라운드 별 비교횟수는 배열 크기의 현재 라운드를 뺀 만큼 비교함
             for (int j = 0; j < size - i; j++) {
+
                 // 현재 원소가 다음 원소보다 클 경우 서로 원소의 위치를 교환
                 if (a[j] > a[j + 1]) {
                     swap(a, j, j + 1);
+                    swapped = true;
                 }
+            }
+
+            // 만약 swap된적이 없다면 이미 정렬되었다는 의미이므로 반복문을 종료
+            if (!swapped) {
+                break;
             }
         }
     }
