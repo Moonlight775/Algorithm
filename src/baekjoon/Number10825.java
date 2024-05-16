5d690ab3693ec286988a7860b46ca2e771f2c085
KwanYoung Moon <pianoguys775@gmail.com>
2024-05-16 16:27:02 +0900

    백준 10825 (국영수, 정렬) - 람다식으로 변경

--- src/baekjoon/Number10825.java
+++ src/baekjoon/Number10825.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Number10825 {
@@ -22,20 +21,17 @@ public class Number10825 {
             }
         }
 
-        Arrays.sort(arr, new Comparator<String[]>() {
-            @Override
-            public int compare(String[] o1, String[] o2) {
-                if (o1[1].equals(o2[1])) {   // 국어 점수가 같으면
-                    if (o1[2].equals(o2[2])) {   // 영어 점수까지 같으면
-                        if (o1[3].equals(o2[3])) {   // 수학 점수까지 같다면
-                            return o1[0].compareTo(o2[0]);  // 모든 점수가 같으면 이름 사전 순
-                        }
-                        return Integer.parseInt(o2[3]) - Integer.parseInt(o1[3]);   // 국어, 영어 점수가 같으면 수학 점수 내림차순 정렬
+        Arrays.sort(arr, (o1, o2) -> {
+            if (o1[1].equals(o2[1])) {   // 국어 점수가 같으면
+                if (o1[2].equals(o2[2])) {   // 영어 점수까지 같으면
+                    if (o1[3].equals(o2[3])) {   // 수학 점수까지 같다면
+                        return o1[0].compareTo(o2[0]);  // 모든 점수가 같으면 이름 사전 순
                     }
-                    return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);   // 국어 점수가 같으면 영어 점수 오름차순 정렬
+                    return Integer.parseInt(o2[3]) - Integer.parseInt(o1[3]);   // 국어, 영어 점수가 같으면 수학 점수 내림차순 정렬
                 }
-                return Integer.parseInt(o2[1]) - Integer.parseInt(o1[1]);   // 국어 점수 기준으로 내림차순 정렬
+                return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);   // 국어 점수가 같으면 영어 점수 오름차순 정렬
             }
+            return Integer.parseInt(o2[1]) - Integer.parseInt(o1[1]);   // 국어 점수 기준으로 내림차순 정렬
         });
 
         StringBuilder sb = new StringBuilder();
