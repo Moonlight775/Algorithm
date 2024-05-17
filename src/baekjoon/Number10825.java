5ba0f8af5d19442a86d8ea7c0225705698ec6f30
KwanYoung Moon <pianoguys775@gmail.com>
2024-05-17 15:34:22 +0900

    백준 10825 (국영수, 정렬) - 메모리와 시간감소를 위해 클래스를 만든 후 정렬

--- src/baekjoon/Number10825.java
+++ src/baekjoon/Number10825.java
@@ -3,40 +3,58 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
+
+class Student {
+    String name;    // 이름
+    int korean;     // 국어
+    int english;    // 영어
+    int math;       // 수학
+
+    // 생성자
+    public Student(String name, int korean, int english, int math) {
+        this.name = name;
+        this.korean = korean;
+        this.english = english;
+        this.math = math;
+    }
+}
 
 public class Number10825 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());    // 학생의 수
-        String[][] arr = new String[N][4];
+
+        ArrayList<Student> list = new ArrayList<>();    //
 
         StringTokenizer st;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 4; j++) {
-                arr[i][j] = st.nextToken();
-            }
+            list.add(new Student(
+                    st.nextToken(), // 이름
+                    Integer.parseInt(st.nextToken()),   // 국어
+                    Integer.parseInt(st.nextToken()),   // 영어
+                    Integer.parseInt(st.nextToken()))); // 수학
         }
 
-        Arrays.sort(arr, (o1, o2) -> {
-            if (o1[1].equals(o2[1])) {   // 국어 점수가 같으면
-                if (o1[2].equals(o2[2])) {   // 영어 점수까지 같으면
-                    if (o1[3].equals(o2[3])) {   // 수학 점수까지 같다면
-                        return o1[0].compareTo(o2[0]);  // 모든 점수가 같으면 이름 사전 순
+        list.sort((o1, o2) -> {
+            if (o1.korean == o2.korean) {   // 국어점수가 같다면
+                if (o1.english == o2.english) { // 영어점수까지 같다면
+                    if (o1.math == o2.math) {   // 모든 점수가 같다면
+                        return o1.name.compareTo(o2.name);  // 이름 사전 순으로 오름차순 정렬
                     }
-                    return Integer.parseInt(o2[3]) - Integer.parseInt(o1[3]);   // 국어, 영어 점수가 같으면 수학 점수 내림차순 정렬
+                    return o2.math - o1.math;   // 수학 점수 내림차순 정렬
                 }
-                return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]);   // 국어 점수가 같으면 영어 점수 오름차순 정렬
+                return o1.english - o2.english; // 영어 점수 오름차순 정렬
             }
-            return Integer.parseInt(o2[1]) - Integer.parseInt(o1[1]);   // 국어 점수 기준으로 내림차순 정렬
+            return o2.korean - o1.korean;   // 국어 점수 내림차순 정렬
         });
 
         StringBuilder sb = new StringBuilder();
-        for (String[] i : arr) {
-            sb.append(i[0]).append('\n');
+        for (Student i : list) {
+            sb.append(i.name).append('\n');
         }
 
         System.out.println(sb);
