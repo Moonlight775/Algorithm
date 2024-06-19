50eaeaadbdd71f12eaea09cbf037542fe90a780b
KwanYoung Moon <pianoguys775@gmail.com>
2024-06-19 17:52:06 +0900

    백준 1260 (DFS 와 BFS, 깊이 우선 탐색 & 너비 우선 탐색) - 인접 리스트에 그래프를 저장 - DFS 를 실행하면서 방문 배열 체크와 탐색 노드 기록을 수행 - 작은 번호의 노드부터 탐색하기 위해서 인접 노드를 오름차순으로 정렬한 후 재귀함수를 호출 - BFS 도 같은 방식으로 진행. 노드를 오름차순으로 정렬하여 큐에 삽입 - DFS 와 BFS 를 마쳤다면 각각 탐색하며 기록한 데이터를 출력

--- src/baekjoon/Number1260.java
+++ src/baekjoon/Number1260.java
@@ -3,70 +3,81 @@ package baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Number1260 {
 
-    public static int N;
-    public static int M;
-    public static int V;
-    public static boolean[] visit;
-    public static int[][] arr;
-    public static Queue<Integer> que = new LinkedList<>();
-    public static StringBuilder sb = new StringBuilder();
+    static boolean[] visited;
+    static ArrayList<Integer>[] A;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        V = Integer.parseInt(st.nextToken());
 
-        arr = new int[N + 1][N + 1];
-        visit = new boolean[N + 1];
+        int N = Integer.parseInt(st.nextToken());       // 노드 개수
+        int M = Integer.parseInt(st.nextToken());       // 에지 개수
+        int Start = Integer.parseInt(st.nextToken());   // 시작점
+        A = new ArrayList[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            A[i] = new ArrayList<>();
+        }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
 
-            arr[x][y] = arr[y][x] = 1;
+            A[S].add(E);
+            A[E].add(S);
         }
 
-        dfs(V);
-        Arrays.fill(visit, false);
-        bfs(V);
+        // 정점 번호가 작은 것을 먼저 방문하기 위해 정렬
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(A[i]);
+        }
+
+        visited = new boolean[N + 1];   // 방문 배열 초기화
+        DFS(Start);
+        sb.append('\n');
+
+        visited = new boolean[N + 1];   // 방문 배열 초기화
+        BFS(Start);
+        sb.append('\n');
+
         System.out.println(sb);
     }
 
-    public static void dfs(int num) {
-        visit[num] = true;
-        sb.append(num).append(" ");
-        for (int i = 1; i <= N; i++) {
-            if (arr[num][i] == 1 && !visit[i]) {
-                dfs(i);
+    // DFS 구현
+    public static void DFS(int Node) {
+        sb.append(Node).append(" ");
+
+        visited[Node] = true;
+
+        for (int i : A[Node]) {
+            if (!visited[i]) {
+                DFS(i);
             }
         }
     }
 
-    public static void bfs(int num) {
-        que.add(num);
-        visit[num] = true;
-        sb.append('\n').append(num).append(" ");
+    // BFS 구현
+    private static void BFS(int Node) {
+        Queue<Integer> que = new LinkedList<>();
+        visited[Node] = true;
+        que.add(Node);
 
         while (!que.isEmpty()) {
-            int idx = que.poll();
-            for (int i = 1; i <= N; i++) {
-                if (arr[idx][i] == 1 && !visit[i]) {
+            int now_Node = que.poll();
+            sb.append(now_Node).append(" ");
+
+            for (int i : A[now_Node]) {
+                if (!visited[i]) {
+                    visited[i] = true;
                     que.add(i);
-                    visit[i] = true;
-                    sb.append(i).append(" ");
                 }
             }
         }
     }
-
 }
