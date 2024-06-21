a2a213d322dbf16138e64d5e50d66b1d05bb21b3
KwanYoung Moon <pianoguys775@gmail.com>
2024-06-21 21:35:54 +0900

    백준 15688 (수 정렬하기 5, 정렬) - 시간 누적이므로 BufferedReader 와 StringBuilder 를 사용하여 입출력에서 시간 단축 - Arrays.sort() 를 사용하여 정렬

---
+++ src/baekjoon/Number15688.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Number15688 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(arr);   // 오름차순(비내림차순) 정렬
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
