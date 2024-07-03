9a503cfed99d4df277cd8883a6d750bf05a3bd64
KwanYoung Moon <pianoguys775@gmail.com>
2024-07-03 23:13:14 +0900

    백준 1012 (유기농 배추, 너비 우선 탐색) - 2차원 배열에 데이터를 저장 - 2차원 배열의 모든 인덱스를 확인하며 숫자가 1이면서 아직 방문하지 않았을 경우 BFS 를 실행 - BFS 를 실행하며 상, 하, 좌, 우 네 방향을 보며 인접한 칸을 보고, 인접한 칸의 숫자가 1이면서 아직 방문하지 않았다면 큐에 삽입하고 방문배열 true 로 저장 - 모든 인덱스를 돌며 BFS 실행 횟수 확인, BFS 실행 횟수가 배추흰지렁이 마리 수이므로 출력

---
+++ src/baekjoon/Number1012.java
@@ -0,0 +1,73 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Number1012 {
+
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    static boolean[][] visited;
+    static int[][] A;
+    static int M, N, K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());   // 가로길이
+            N = Integer.parseInt(st.nextToken());   // 세로길이
+            K = Integer.parseInt(st.nextToken());   // 배추 개수
+            A = new int[M][N];
+            visited = new boolean[M][N];
+            int count = 0;
+
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                A[x][y] = 1;
+            }
+
+            for (int i = 0; i < M; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (A[i][j] == 1 && !visited[i][j]) {
+                        bfs(i, j);
+                        count++;
+                    }
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
+
+    public static void bfs(int i, int j) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{i, j});
+        visited[i][j] = true;
+
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+
+            for (int k = 0; k < 4; k++) {
+                int x = now[0] + dx[k];
+                int y = now[1] + dy[k];
+
+                if (x >= 0 && y >= 0 && x < M && y < N) {   // 좌표 유효성 검사
+                    if (A[x][y] != 0 && !visited[x][y]) {
+                        visited[x][y] = true;
+                        queue.add(new int[]{x, y});
+                    }
+                }
+            }
+        }
+    }
+}
