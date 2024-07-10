91801deb8c366421b5ee98f73a3fae824e49518e
KwanYoung Moon <pianoguys775@gmail.com>
2024-07-10 09:06:09 +0900

    백준 1463 (1로 만들기, 다이나믹 프로그래밍) - 바텀-업 방식으로 구현 - 점화식의 형태와 의미를 도출  -- D[i]: i에서 1로 만드는 데 걸리는 최소 연산 횟수 - 점화식  -- D[i] = D[i - 1] + 1 : 1을 빼는 연산이 가능  -- if(i % 2 == 0) D[i] = min(D[i], D[i / 2] + 1) : 2로 나누는 연산이 가능  -- if(i % 3 == 0) D[i] = min(D[i], D[i / 3] + 1 : 3으로 나누는 연산이 가능 - 점화식을 이용해 D 배열을 채우고, D[N]을 출력

--- src/baekjoon/Number1463.java
+++ src/baekjoon/Number1463.java
@@ -1,44 +1,31 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Number1463 {
 
-    static Integer[] dp;
+    static int N;
+    static int[] D;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int N = Integer.parseInt(br.readLine());
+        N = sc.nextInt();
+        D = new int[N + 1];
+        D[1] = 0;
 
-        dp = new Integer[N+1];
-        dp[0] = dp[1] = 0;
+        for (int i = 2; i <= N; i++) {
+            D[i] = D[i - 1] + 1;    // - 1 연산 표현
 
-        System.out.println(recur(N));
-    }
-
-    static int recur(int N) {
-        if (dp[N] == null) {
-            // 6으로 나눠질 때 (3, 2 둘 다 나눠질 때)
-            if (N%6 == 0) {
-                dp[N] = Math.min(recur(N-1), Math.min(recur(N/3), recur(N/2))) + 1;
+            if (i % 2 == 0) {   // 나누기 2 연산
+                D[i] = Math.min(D[i], D[i / 2] + 1);
             }
-            // 3으로만 나눠질 때
-            else if (N%3 == 0) {
-                dp[N] = Math.min(recur(N/3), recur(N-1)) + 1;
-            }
-            // 2로만 나눠질 때
-            else if (N%2 == 0) {
-                dp[N] = Math.min(recur(N/2), recur(N-1)) + 1;
-            }
-            // 2와 3으로 나눠지지 않을 때
-            else {
-                dp[N] = recur(N-1) + 1;
+
+            if (i % 3 == 0) {   // 나누기 3 연산
+                D[i] = Math.min(D[i], D[i / 3] + 1);
             }
         }
 
-        return dp[N];
+        System.out.println(D[N]);
     }
 }
